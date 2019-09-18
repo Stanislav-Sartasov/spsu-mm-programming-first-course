ce646a1b2f1fbd23bd1941ae33790d46bfbfb26f
ToxaKaz <anton.a.kazancev@gmail.com>
2019-09-19 02:49:40 +0300

    Task4.c   - algorithm speed updated, using Mersenne numbers Theorems

--- Solutions/Task4/Task4.c
+++ Solutions/Task4/Task4.c
@@ -1,22 +1,35 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+
+char std_prime(int in)
+{
+	if ((!(in % 2) || in == 1) && (in != 2))
+		return 0;
+	for (int i = 3; i * i <= in; i += 2)
+		if (!(in % i))
+			return 0;
+	return 1;
+}
 
 int main()
 {
-	int Mars = 0;			//for Mersenne numbers
+	int Mars = 0;
 	for (int i = 1; i <= 31; i++)
 	{
-		Mars = (Mars << 1) | 1; //Marsenne number for i because Marsenne number = 111...111 in binary
-		char flag = 1;			//flag for division finding
-		for (int j = i + 1; j < Mars / 2; j += i)	//finding division of Marsenne number
-		{											//Marsenne number division = 1(mod i) by theoreme
-			if (Mars % j == 0)
+		Mars = (Mars << 1) | 1;
+		if (std_prime(i))		//if 2^i − 1 is prime, then i is prime
+		{
+			char flag = 1;		//j that divides 2^i − 1 must be 1 + 2*i*k where k - natural
+			for (int j = 2 * i + 1; (long long)j * (long long)j <= Mars; j += 2 * i)
 			{
-				flag = 0;
-				break;
+				if (!(Mars % j))
+				{
+					flag = 0;
+					break;
+				}
 			}
+			if (flag)
+				printf("%d\n", Mars);
 		}
-		if (flag)
-			printf("%d\n", Mars);
 	}
 
 	return 0;
