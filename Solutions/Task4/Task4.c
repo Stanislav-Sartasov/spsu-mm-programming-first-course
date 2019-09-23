43d1909e1b4f03fd3dc0472a1328ed5560d180f4
ToxaKaz <anton.a.kazancev@gmail.com>
2019-09-23 17:41:30 +0300

    Task4.c  - paragraph 16 of code-style fix ("Mars" -> "mersenne_number")

--- Solutions/Task4/Task4.c
+++ Solutions/Task4/Task4.c
@@ -12,23 +12,23 @@ char std_prime(int in)
 
 int main()
 {
-	int Mars = 0;
+	int mersenne_number = 0;
 	for (int i = 1; i <= 31; i++)
 	{
-		Mars = (Mars << 1) | 1;
+		mersenne_number = (mersenne_number << 1) | 1;
 		if (std_prime(i))		//if 2^i − 1 is prime, then i is prime
 		{
 			char flag = 1;		//j that divides 2^i − 1 must be 1 + 2*i*k where k - natural
-			for (int j = 2 * i + 1; (long long)j * (long long)j <= Mars; j += 2 * i)
+			for (int j = 2 * i + 1; (long long)j * (long long)j <= mersenne_number; j += 2 * i)
 			{
-				if (!(Mars % j))
+				if (!(mersenne_number % j))
 				{
 					flag = 0;
 					break;
 				}
 			}
 			if (flag)
-				printf("%d\n", Mars);
+				printf("%d\n", mersenne_number);
 		}
 	}
 
