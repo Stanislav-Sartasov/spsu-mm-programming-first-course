4c9e709227d2a36cfb12daebb3ba3a007fbc1856
Lyubov Miloserdova <55358539+miloserdova-l@users.noreply.github.com>
2019-09-23 23:41:04 +0300

    - исправлен кодстайл

--- task4/main.c
+++ task4/main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-int main() {
+int main()
+{
     int n = 2;
     long long cur = 2;
-    while (n < 32) {
+    while (n < 32)
+    {
         cur *= 2;
-        n++;
         int fl = 1;
-        for (int i = 2; i < (int)sqrt(cur - 1) + 1; i++) {
-            if ((cur - 1) % i == 0) {
+        for (int i = 2; i < (int)sqrt(cur - 1) + 1; i++)
+        {
+            if ((cur - 1) % i == 0)
+            {
                 fl = 0;
                 break;
             }
         }
         if (fl)
             printf("%lld\n", cur - 1);
+        n++;
     }
     return 0;
 }
