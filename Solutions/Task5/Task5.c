4a0aa8bb87e1e22da4033262aaadaeb4aad86735
ToxaKaz <anton.a.kazancev@gmail.com>
2019-09-23 23:49:05 +0300

    Task5.c  - algorithm change  - use inputing.h https://github.com/Stanislav-Sartasov/spsu-mm-programming-first-course/commit/9e541fe6e136cf4142e23447f5ead7278eca89db

--- Solutions/Task5/Task5.c
+++ Solutions/Task5/Task5.c
@@ -1,131 +1,75 @@
 #include <stdio.h>
 #include <math.h>
-#define LDBL_TRUE_MIN  4.9406564584124654E-324
+#include <malloc.h>
+#include "inputing.h"
 
-void in_double(char* massage, double* variable_double)
+int* set_len(int* array, int* size, int new_size)
 {
-	char c;
-in_double_begin:
-	printf("%s", massage);
-
-	do
+	int* auxiliary_array = (int*)malloc(new_size * sizeof(int));
+	while (auxiliary_array == '\0')
+		auxiliary_array = (int*)malloc(new_size * sizeof(int));
+	if (*size)
 	{
-		c = getchar();
-	} while (c == ' ' || c == '\t');
-
-	if (c == '\n')
-		goto in_double_begin;
-
-	short minus;
-	if (c == '-')
-	{
-		c = getchar();
-		minus = -1;
+		for (int i = 0; (i < *size) || (i < new_size); i++)
+			auxiliary_array[i] = array[i];
+		free(array);
 	}
-	else
-		minus = 1;
-
-	while (c == ' ' || c == '\t')
-		c = getchar();
-
-	*variable_double = 0.0;
-	char whole_part_flag = (c >= '0' && c <= '9') ? 1 : 0;
-
-	while (c >= '0' && c <= '9')
-	{
-		*variable_double = *variable_double * 10 + c - '0';
-		c = getchar();
-	}
-
-	while (c == ' ' || c == '\t')
-		c = getchar();
-
-	if ((c == '.') && whole_part_flag)
-	{
-		c = getchar();
-
-		while (c == ' ' || c == '\t')
-			c = getchar();
-
-		if (c < '0' || c > '9')
-		{
-			if (c != '\n')
-				while (getchar() != '\n');
-			printf("invalid input\n");
-			goto in_double_begin;
-		}
-		for (int i = 10; c >= '0' && c <= '9'; i *= 10)
-		{
-			*variable_double = *variable_double + ((double)c - (double)'0') / (double)i;
-			c = getchar();
-		}
-	}
-
-	while (c == ' ' || c == '\t')
-		c = getchar();
-
-	if (c != '\n')
-	{
-		while (getchar() != '\n');
-		printf("invalid input\n");
-		goto in_double_begin;
-	}
-
-	*variable_double *= minus;
-}
-
-long double f(int* array, int i, int size)
-{
-	if (!i)
-		return array[size];
-	return array[size - i] + 1 / f(array, i - 1, size);
-}
-
-long double cfin(int* array, int size)	//continued fraction in number
-{
-	return f(array, size, size);
+	*size = new_size;
+	return auxiliary_array;
 }
 
 int main()
 {
 	printf("This program for the entered number determines the continued fraction of its square root\n\n");
-	double n;
-	in_double("Enter number: ", &n);
-	printf("\n");
-	long double in = sqrtl(n);
-	int a[100];
-	a[0] = truncl(in);
-	in -= a[0];
-	printf("%d", a[0]);
-	for (int i = 1; i < 100; i++)
+	int n;
+	do
 	{
-		if (fabsl(cfin(a, i - 1) - sqrtl(n)) <= LDBL_TRUE_MIN)
-		{
-			printf("\n\nProgram can't find continued fraction for nuber you entered because of long double accuracy,\n");
-			printf("Or number you entered is squared number, and this is final continued fraction for it\n");
-			return 0;
-		}
-		a[i] = truncl(1 / in);
-		in = 1 / in - a[i];
-		printf(", %d", a[i]);		
-		if (a[i] == a[0] * 2)
+		in_int("Enter number: ", &n);
+		if (n <= 0)
+			printf("positive value expected\n");
+	}
+	while (n <= 0);
+	printf("\n");
+
+	int whole_part = trunc(sqrt(n));
+
+	printf("%d", whole_part);
+
+	if (trunc(whole_part) * trunc(whole_part) == n)
+	{
+		printf("\n");
+		return 0;
+	}
+
+	int t_fraction = 1;
+	int b_fraction = 0; // top fraction, bottom fraction
+
+	int* c_fraction = 0;
+	int c_fraction_len = 0;
+
+	for (int i = 1;; i++)
+	{
+		int t = whole_part - b_fraction;	//intermediate variable
+		b_fraction = t + whole_part;
+		t_fraction = (n - t * t) / t_fraction;
+		c_fraction = set_len(c_fraction, &c_fraction_len, i);
+		c_fraction[i - 1] = b_fraction / t_fraction;
+		printf(", %d", c_fraction[i - 1]);
+		b_fraction = b_fraction % t_fraction;
+		if (c_fraction[i - 1] == whole_part * 2)
 		{
 			char flag = 1;
-			for (int j = 1; j <= i / 2; j++)
-			{
-				if (a[j] != a[i - j])
+			for (int j = 0; j < c_fraction_len / 2; j++)
+				if (c_fraction[j] != c_fraction[c_fraction_len - 2 - j])
 				{
 					flag = 0;
 					break;
 				}
-			}
 			if (flag)
 			{
-				printf("\n");
+				printf("\n\n");
 				return 0;
 			}
 		}
 	}
-	printf("\nProgram can't find continued fraction for nuber you entered because of long double accuracy\n");
-	return -1;
 }
