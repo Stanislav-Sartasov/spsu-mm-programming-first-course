59b2c5c399bc6312841e4979c4a14035e1a814db
ToxaKaz <anton.a.kazancev@gmail.com>
2019-09-23 17:15:46 +0300

    - Task2.c    - using inputing.h for in_int()    - variable constraints fix

--- Solutions/Task2/Task2.c
+++ Solutions/Task2/Task2.c
@@ -1,58 +1,5 @@
 #include <stdio.h>
-
-
-
-void in_int(char* massage, int* variable_int)	//function for int input
-{
-	char c;
-in_int_begin:
-	printf("%s", massage);
-	do
-	{								//handling spaces at the beginning of a line
-		c = getchar();
-	}
-	while (c == ' ' || c == '\t');
-
-	if (c == '\n')
-		goto in_int_begin;		//handling empty input
-
-	char minus;						//handling negative input
-	if (c == '-')
-	{
-		c = getchar();
-		minus = -1;
-	}
-	else
-		minus = 1;
-
-	while (c == ' ' || c == '\t')
-		c = getchar();
-
-	*variable_int = 0;
-
-	while (c >= '0' && c <= '9')
-	{
-		if (*variable_int * 10 + c - '0' < 0)		//handling int overflow
-		{
-			while (getchar() != '\n');
-			printf("invalid input, input is too big\n");
-			goto in_int_begin;
-		}
-		*variable_int = *variable_int * 10 + c - '0';
-		c = getchar();
-	}
-
-	while (c == ' ' || c == '\t')		//post processing and validation of input
-		c = getchar();
-
-	if (c != '\n')
-	{
-		while (getchar() != '\n');
-		printf("invalid input\n");
-		goto in_int_begin;
-	}
-	*variable_int *= minus;
-}
+#include "inputing.h"
 
 int gcd(int a, int b)			//greatest common divisor
 {
@@ -78,7 +25,7 @@ int main()
 		if (x <= 0)
 			printf("invalid input, natural number expacted\n");
 	}
-	while (x < 0);
+	while (x <= 0);
 	printf("\n");
 
 	do
@@ -87,7 +34,7 @@ int main()
 		if (y <= 0)
 			printf("invalid input, natural number expacted\n");
 	}
-	while (y < 0);
+	while (y <= 0);
 	printf("\n");
 
 	do
@@ -96,7 +43,7 @@ int main()
 		if (z <= 0)
 			printf("invalid input, natural number expacted\n");
 	}
-	while (z < 0);
+	while (z <= 0);
 	printf("\n");
 	
 	if (x > y)
