be0a4b364e1155bc5c01fa7ca01901478b299406
BakaevEvgeniy <evgeniybakaev@bk.ru>
2019-10-15 17:37:48 +0300

    task 10, нерекурсивное решение

--- task10/task10.c
+++ task10/task10.c
@@ -1,52 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 int correct_input(char str[])
 {
 	char digits[11] = "0123456789";
-	int digit_true, exit = 0;
+	int digit_true;
 	if (str[0] != '0')
 	{
 
-		for (int l = 0; (l < strlen(str)) && (exit != 1); l++)
+		for (int l = 0; (l < strlen(str)); l++)
 		{
 			digit_true = 0;
-			for (int n = 0; (n < 10) && (exit != 1); n++)
+			for (int n = 0; (n < 10); n++)
 			{
 				if (str[l] == digits[n]) digit_true = 1;
 			}
-			if (digit_true == 0) exit = 1;
+			if (digit_true == 0) return 1;
 		}
 	}
-	else exit = 1;
-	if (exit == 1) return 1;
-	else return 0;
-}
-
-// ( ол-во способов представить сумму total всеми видами монет) ==
-// ((представить total без монеты coin) + ( представить (total - coin) всеми видами монет)) 
-int count(int total, int coins[], int amount) 
-{
-	if (total == 0) return 1;
-	if (total < 0) return 0;
-	if ((amount <= 0) && (total >= 1)) return 0;
-	return (count(total, coins, (amount - 1)) + count((total - coins[(amount - 1)]), coins, amount));
+	else return 1;
+	return 0;
 }
 
 int main()
 {
-	int a[8] = { 1, 2, 5, 10, 20, 50, 100, 200 };
-	int sum = 0;
+	int coins[8] = { 1, 2, 5, 10, 20, 50, 100, 200 };
+	int sum;
 	int j = 0;
+	int amount;
+	int* table;
 	char s[21];	
+	
+	amount = sizeof(coins) / sizeof(coins[0]);
+	printf("Enter the required amount: ");
+	
 	while (j !=	1)
 	{
 		(void)scanf("%s", s);
 		if (correct_input(s) != 0) printf("Incorrect input. Enter a positive integer.\n");
 		else j = 1;
 	}
+
 	sum = atoi(s);
-	printf("%d", count(sum, a, 8));
+	table = (int*)malloc((sum + 1) * sizeof(int));
+	if (table == NULL)
+	{
+		printf("Memory not allocated.\n");
+		exit(0);
+	}
+	else
+	{
+		memset(table, 0, (sum + 1) * sizeof(int));
+		table[0] = 1;
+
+		// ( ол-во способов представить сумму S всеми видами монет) ==
+		// ((представить сумму S без монеты coins[i]) + ( представить (S - coins[i]) всеми видами монет)) 
+		for (int i = 0; i < amount; i++)
+		{
+			for (int k = coins[i]; k <= sum; k++)
+			{
+			table[k] += table[k - coins[i]];
+			}
+		}
+		printf("%d\n", table[sum]);
+	}
+	free(table);
 	return 0;
 }
