1d0f2b706822c8d087c587bbfbfe768bd67b5407
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2019-10-29 18:35:19 +0300

    Add files via upload

--- HomeWork/3d Task/3d Task/Source.c
+++ HomeWork/3d Task/3d Task/Source.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-void Tran(float a, float b, float c,unsigned int &isTran)
+
+unsigned int Trian(float a, float b, float c)       // is possible to bild a triangle if yes isTrian = 1
 {
-	if ((a + b > c) && (a + c > b) && (b + c > a)) isTran = 1;
-	else  isTran = 0;
+	if ((a + b > c) && (a + c > b) && (b + c > a)) return 1;
+	else  return 0;
 }
+
 void angle(float a, float b, float c)
 {
 	float cosa;
@@ -17,9 +19,9 @@ void angle(float a, float b, float c)
 int main()
 {
 	system("chcp 1251");
-	float a,b,c;
+	float a, b, c;
 	int res;
-	unsigned int isTran = 0;
+	unsigned int isTrian = 0;
 	printf("%s", "Enter lengths of sides of the triangle ");
 	do
 	{
@@ -28,26 +30,24 @@ int main()
 		while (getchar() != '\n');
 		if (res == 3) break;
 		else printf("%s", "Invalid numbers entered, try again\n");
-	} 
-	while (res != 3);
+	} while (res != 3);
 
-	Tran(a,b,c,isTran);
-	printf("%hu",isTran);
+	if (Trian(a, b, c) == 0) printf("%s\n", "Is not tiangle");
+	else
+	{
+		float cosa, cosb, cosc;
+		cosa = (a*a - b * b - c * c) / ((-2)*b*c);
+		cosa = acos(cosa) * 180 / 3.14159265359;
 
+		cosb = (b*b - a * a - c * c) / ((-2)*a*c);
+		cosb = acos(cosb) * 180 / 3.14159265359;
 
-	float cosa,cosb,cosc;
-	cosa = (a*a - b * b - c * c) / ((-2)*b*c);
-	cosa = acos(cosa) * 180 / 3.14159265359;
-
-	cosb = (b*b - a * a - c * c) / ((-2)*a*c);
-	cosb = acos(cosb) * 180 / 3.14159265359;
-
-	cosc = (c*c - b * b - a * a) / ((-2)*b*a);
-	cosc = acos(cosc) * 180 / 3.14159265359;
-	printf("\n%f", cosa);
-	printf("\n%f", cosb);
-	printf("\n%f", cosc);
-
+		cosc = (c*c - b * b - a * a) / ((-2)*b*a);
+		cosc = acos(cosc) * 180 / 3.14159265359;
+		printf("\n%f", cosa);
+		printf("\n%f", cosb);
+		printf("\n%f", cosc);
+	}
 
 	system("pause");
 	return 0;
