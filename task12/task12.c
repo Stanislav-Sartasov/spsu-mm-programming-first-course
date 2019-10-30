a3ca38aa484a2838b6090a20d6e7c3fe0f14cf76
BakaevEvgeniy <evgeniybakaev@bk.ru>
2019-10-30 12:35:24 +0300

    task 12

--- task12/task12.c
+++ task12/task12.c
@@ -1,48 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+int digit_amount(int base, int exp, int a) //  ол-во цифр в числе (base ^ exp) в системе счислени€ a. 
+{
+	return ceil(exp * (log(base) / log(a))); // ћинимальное решение нер-ва (base ^ exp) < (a ^ x) дл€ целого x. 
+
+}
 
 int main()
 {
-	int list[335]; // 3^5000 < 16^2000, в блоке будем записывать остаток от делени€ на 16^6, т.е. шестизначное шестнадцатиричное число. 2000/6 ~ 335
+	int* list;
 	int i = 5000;
-	int j = 334;
+	int j = 0;
+	int l;
 	int rest;
-	int zero_amount = -1;
 	int q = 0;
 	int unit = 16777216; // = 16^6
-	memset(list, 0, 335 * sizeof(int));
-	list[334] = 1;
-	printf("3^5000 in a hexadecimal number system has the form:\n");
-	while (i > 0)
+	
+	j = ceil(digit_amount(3, 5000, 16) / 6);
+	list = (int*)malloc((j + 1) * sizeof(int));
+	if (list == NULL)
 	{
-		rest = 0;
-		q = 0;
-		j = 334;
-		while (j >= 0)
+		printf("Memory not allocated.\n");
+		exit(0);
+	}
+	else
+	{
+		memset(list, 0, (j + 1) * sizeof(int));
+		list[j] = 1;
+		printf("3^5000 in a hexadecimal number system has the form:\n");
+		l = j;
+		while (i > 0)
 		{
-			list[j] = list[j] * 3 + q;
-			
-			rest = list[j] % unit;
-			q = (list[j] - rest) / unit;
-			list[j] = rest;
-			
-			j -= 1;
+			rest = 0;
+			q = 0;
+			j = l;
+			while (j >= 0)
+			{
+				list[j] = list[j] * 3 + q;
+
+				rest = list[j] % unit;
+				q = (list[j] - rest) / unit;
+				list[j] = rest;
+
+				j -= 1;
+			}
+
+			i -= 1;
 		}
-		
-		i -= 1;
-	}
 
-	int l = 0;
-	while (list[l] == 0)
-	{
-		zero_amount = l;
-		l += 1;
+		for (int k = 0; k <= l; k++)
+		{
+			if (k != 0)	printf("%06x", list[k]);  //  аждый "блок" представл€ем в шестнадцатиричной системе с фиксированным числом знаков, т.к. хотим "склеить" блоки
+			else printf("%x", list[k]);
+		}
 	}
-
-	for (int k = zero_amount + 1; k < 335; k++)
-	{
-		printf("%06x", list[k]);  //  аждый "блок" представл€ем в шестнадцатиричной системе с фиксированным числом знаков, т.к. хотим "склеить" блоки
-	}
-
+	free(list);
 	return 0;
 }
