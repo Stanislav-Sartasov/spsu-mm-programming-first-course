94cf2762fb2893e00a7acbfd0534980c53131cb8
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2019-11-11 22:19:15 +0300

    Update Source.c

--- HomeWork/Task 12/12th task/Source.c
+++ HomeWork/Task 12/12th task/Source.c
@@ -1,11 +1,11 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include <stdlib.h>
 
 void myPow(int *mas, int n, int pow)
 {
 	int i, j;
-	for (i = 0; i < pow ; i++)
+	for (i = 0; i < pow; i++)
 	{
 		for (j = 0; j < i; j++) mas[j] *= n;
 		for (j = 0; j < i; j++)
@@ -17,7 +17,7 @@ void myPow(int *mas, int n, int pow)
 	}
 
 	for (j = 0; j < pow; j++) mas[j] *= n;
-	for (j = 0; j < pow-1; j++)
+	for (j = 0; j < pow - 1; j++)
 		if (mas[j] >= 16)
 		{
 			mas[j + 1] += mas[j] / 16;
@@ -28,7 +28,7 @@ void myPow(int *mas, int n, int pow)
 int main()
 {
 	int n = 5000, i;
-	int mas[5000];
+	int *mas = (int*)malloc(n * sizeof(int));
 	mas[0] = 1;
 	for (i = 1; i < n; i++) mas[i] = 0;
 
@@ -44,7 +44,7 @@ int main()
 
 	for (i = zero - 1; i >= 0; i--) printf("%x", mas[i]);
 	printf("\n");
-
+	free(mas);
 	system("pause");
 	return 0;
 }
