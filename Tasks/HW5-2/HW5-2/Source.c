9c8fec451d6cc1f07152085b08a8dd78ce50203b
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2019-12-02 20:43:10 +0300

    Add files via upload

--- Tasks/HW5-2/HW5-2/Source.c
+++ Tasks/HW5-2/HW5-2/Source.c
@@ -1,77 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
-#include <math.h>
-#include <stdbool.h>
-
-int isPrime(int num)
-{
-	int d;
-
-	for (d = 2; d * d <= num; d++)
-	{
-		if (num % d == 0)
-		{
-			return(0);
-		}
-	}
-
-	return(1);
-}
-
-
-int digitRoot(int num)
-{
-	int sum;
-
-	while (num > 9)
-	{
-		sum = 0;
-
-		while (num > 0)
-		{
-			sum = sum + num % 10;
-			num = num / 10;
-		}
-
-		num = sum;
-	}
-
-	return(num);
-}
 
 int main()
 {
-	int sum_MDRS = 0;
-	int* digit;
-	digit = (int*)malloc(1000000 * sizeof(int));
 
-	for (int n = 2; n < 1000000; n++) 
+	int* MDRS = (int*)malloc(sizeof(int) * 1000000);
+	int sum = 0;
+	for (int i = 2; i < 1000000; i++)
+		MDRS[i] = (i - 1) % 9 + 1;
+	for (int i = 2; i < 1000000; i++)
 	{
-		if ((n < 10) || isPrime(n) == 1)
+		int j = 1;
+		sum += MDRS[i];
+		while (j * i < 1000000)
 		{
-			digit[n] = digitRoot(n);
-			sum_MDRS = sum_MDRS + digit[n];
-		}
-		else
-		{
-			int mdrs = digitRoot(n);
-
-			for (int i = 2; i * i < n; i++)
-			{
-				if (n % i == 0)
-					digit[n] = digit[i] + digit[n / i];
-
-				if (digit[n] > mdrs)
-					mdrs = digit[n];
-			}
-
-			digit[n] = mdrs;
-			sum_MDRS = sum_MDRS + digit[n];
+			if (MDRS[i * j] < MDRS[i] + MDRS[j])
+				MDRS[i * j] = MDRS[i] + MDRS[j];
+			j++;
 		}
 	}
+	free(MDRS);
+	printf("Sum of all MDRS is %d\n", sum);
 
-	printf("The sum of all MDRS is: %d", sum_MDRS);
-
-	return(0);
+	return 0;
 }
