cd4f27dceacd308f0894d6f36e024e97b8e3c648
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2019-12-04 13:45:22 +0300

    Add files via upload

--- Tasks/HW1-3/HW1-3/Source.c
+++ Tasks/HW1-3/HW1-3/Source.c
@@ -1,22 +1,24 @@
+#define M_PI 3.14159265358979323846
 #include <stdio.h>
 #include <math.h>
-#define M_PI 3.14159265358979323846
 
-void degrees(double a, double b, double c) 
+
+void degrees(float a, float b, float c) 
 {
 	printf("degenerate triangle\n");
-	double alpha = acos((pow(a, 2.0) + pow(b, 2.0) - pow(c, 2.0)) / (2.0 * a * b)) * (180.0 / M_PI);
-	double beta = acos((pow(a, 2.0) + pow(c, 2.0) - pow(b, 2.0)) / (2.0 * a * c)) * (180.0 / M_PI);
-	double gamma = acos((pow(c, 2.0) + pow(b, 2.0) - pow(a, 2.0)) / (2.0 * c * b)) * (180.0 / M_PI);
+	float alpha = acos((pow(a, 2.0) + pow(b, 2.0) - pow(c, 2.0)) / (2.0 * a * b)) * (180.0 / M_PI);
+	float beta = acos((pow(a, 2.0) + pow(c, 2.0) - pow(b, 2.0)) / (2.0 * a * c)) * (180.0 / M_PI);
+	float gamma = acos((pow(c, 2.0) + pow(b, 2.0) - pow(a, 2.0)) / (2.0 * c * b)) * (180.0 / M_PI);
 
-	printf("%lf deg, %lf min, %f sec;\n%lf deg, %lf min, %lf sec;\n%lf deg, %lf min, %lf sec;", alpha, alpha * 60.0, alpha * 3600.0, beta, beta * 60.0, beta * 3600.0, gamma, gamma * 60.0, gamma * 3600.0);
+	printf("%f deg, %f min, %f sec;\n%f deg, %f min, %f sec;\n%f deg, %f min, %f sec;", alpha, (int)alpha * 60.0, (int)alpha * 3600.0, beta, (int)beta * 60.0, (int)beta * 3600.0, gamma, (int)gamma * 60.0, (int)gamma * 3600.0);
 }
 
 int main() 
 {
-	double a, b, c;
-	scanf_s("%lf%lf%lf", &a, &b, &c);
-	printf("%lf %lf %lf\n\n", a, b, c);
+	printf("Enter sides of triangle to find angles : \n");
+	float a, b, c;
+	scanf_s("%f%f%f", &a, &b, &c);
+	printf("%f %f %f\n\n", a, b, c);
 
 	if (a + b > c && a + c > b && c + b + a) 
 	{
@@ -28,3 +30,4 @@ int main()
 	}
 	return 0;
 }
+
