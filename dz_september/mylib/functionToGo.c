d564d84b82c4fb41ba0a21ad62153e3f54941002
makar-pelogeiko <m.pelogeiko@mail.ru>
2019-12-06 03:02:06 +0300

    задача "Английские монеты" 1) решение задачи 2) поправка кодстайла библиотеки functionToGo.h

--- dz_september/mylib/functionToGo.c
+++ dz_september/mylib/functionToGo.c
@@ -1,14 +1,12 @@
-#pragma once
-#include <stdio.h>
 #include "functionToGo.h"
 
 
-char stringOfAgainInput[80]="try again";
+char stringOfAgainInput[80] = "try again";
 
 long long wholep(double dividend, double divisor) // only for numbers >0)
 {
     long long i=0;
-    while(dividend-divisor > 0)
+    while (dividend - divisor > 0)
     {
         dividend -= divisor;
         ++i;
@@ -18,7 +16,7 @@ long long wholep(double dividend, double divisor) // only for numbers >0)
 
 double reminder(double dividend, double divisor) // only for numbers >0
 {
-    while(dividend-divisor > 0)
+    while (dividend - divisor > 0)
     {
         dividend -= divisor;
     }
@@ -28,11 +26,11 @@ double reminder(double dividend, double divisor) // only for numbers >0
 long long gcd(long long x, long long y)
 {
     long long d = 1;
-    if(x < y)
+    if (x < y)
     {
         d = y; y = x; x = d;
     }
-    while(d)
+    while (d)
     {
         d = x % y;
         x = y;
@@ -42,111 +40,127 @@ long long gcd(long long x, long long y)
 }
 
 
-double savein()
+double saveIn()
 {
-    double a=0, delim=1;
+    double a = 0, delim = 1;
     char c = '\0';
-    int first=1, sign=1, goodset=1, less1=0;
+    int first = 1, sign = 1, goodset = 1, less1 = 0;
 
     do
     {
-        a=0; delim=1;
+        a = 0; delim = 1;
         c = '\0';
-        first=1; sign=1; goodset=1; less1=0;
-        while((c != ' ') && (c != '\n') && goodset)
+        first = 1; sign = 1; goodset = 1; less1 = 0;
+        while ((c != ' ') && (c != '\n') && goodset)
         {
             scanf("%c", &c);
-            if(first)
+            if (first)
             {
-                first=0;
-                if(c == '-')
-                    { sign = -1; continue; }
-                if((c >= '0') && (c <= '9'))
-                    {a=c-'0'; continue; }
+                first = 0;
+                if (c == '-')
+                {
+                    sign = -1; continue;
+                }
+                if ((c >= '0') && (c <= '9'))
+                {
+                    a = c - '0'; continue;
+                }
 
                 goodset = 0;
                 break;
             }
 
-            if(c == '.')
-                { less1 = 1; continue; }
+            if (c == '.')
+            {
+                less1 = 1; continue;
+            }
 
-            if((c >= '0') && (c <='9') && !less1)
+            if ((c >= '0') && (c <='9') && !less1)
             {
                 a = a * 10 + c - '0';
                 continue;
             }
 
-            if((c >= '0') && (c <='9') && less1)
+            if ((c >= '0') && (c <='9') && less1)
             {
                 delim = delim * 10;
-                a = a + (double)(( c - '0' ))/delim;
+                a = a + (double)(( c - '0' )) / delim;
                 continue;
             }
-            if((c != ' ') && (c != '\n'))
-                { goodset = 0;  break; }
+            if ((c != ' ') && (c != '\n'))
+            {
+                goodset = 0;  break;
+            }
         }
         a = a * sign;
-        if(!goodset)
+        if (!goodset)
         {
             printf("%s\n", stringOfAgainInput);
-            c='\0';
+            c = '\0';
             while(c != '\n')
-                scanf("%c",&c);
+                scanf("%c", &c);
         }
-    }while(goodset != 1);
+    }
+    while (goodset != 1);
     return a;
 }
 
-long long saveinint()
+long long saveInInt()
 {
-    long long a=0;
+    long long a = 0;
     char c = '\0';
-    int first=1, sign=1, goodset=1;
+    int first = 1, sign = 1, goodset = 1;
 
     do
     {
-        a=0;
+        a = 0;
         c = '\0';
-        first=1; sign=1; goodset=1;
-        while((c != ' ') && (c != '\n') && goodset)
+        first = 1; sign = 1; goodset = 1;
+        while ((c != ' ') && (c != '\n') && goodset)
         {
             scanf("%c", &c);
-            if(first)
+            if (first)
             {
-                first=0;
-                if(c == '-')
-                    { sign = -1; continue; }
-                if((c >= '0') && (c <= '9'))
-                    {a=c-'0'; continue; }
+                first = 0;
+                if (c == '-')
+                {
+                    sign = -1; continue;
+                }
+                if ((c >= '0') && (c <= '9'))
+                {
+                    a = c-'0'; continue;
+                }
 
                 goodset = 0;
                 break;
             }
 
-            if((c >= '0') && (c <='9'))
+            if ((c >= '0') && (c <= '9'))
             {
                 a = a * 10 + c - '0';
                 continue;
             }
 
-            if((c != ' ') && (c != '\n'))
-                { goodset = 0;  break; }
+            if ((c != ' ') && (c != '\n'))
+            {
+                goodset = 0;  break;
+            }
         }
         a = a * sign;
         if(!goodset)
         {
             printf("%s\n",stringOfAgainInput);
-            c='\0';
-            while(c != '\n')
+            c = '\0';
+            while (c != '\n')
                 scanf("%c",&c);
         }
-    }while(goodset != 1);
+    }
+    while (goodset != 1);
     return a;
 }
 
 void changeAgainInputText(const char *str)
 {
-    for(register int i=0; i<80; ++i)
-        stringOfAgainInput[i]=str[i];
+    for (register int i = 0; i < 80; ++i)
+        stringOfAgainInput[i] = str[i];
 }
--- dz_september/mylib/functionToGo.h
+++ dz_september/mylib/functionToGo.h
@@ -1,10 +1,11 @@
 #pragma once
+#include <stdio.h>
 
 extern char stringOfAgainInput[80];
 
-double savein(void);
-long long saveinint(void);
+double saveIn(void);
+long long saveInInt(void);
 void changeAgainInputText(const char *str);
 long long gcd(long long, long long);
-double reminder(double, double); // only for numbers >0
-long long wholep(double, double); // only for numbers >0)
+double reminder(double, double); // only for numbers >0, divident - wholep(divident, divisor)
+long long wholep(double, double); // only for numbers >0, whole part divident / divisor
