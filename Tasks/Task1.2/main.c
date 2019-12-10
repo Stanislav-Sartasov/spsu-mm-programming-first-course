2ba45c7ff6ea4c1039de542059adb73e238fe6e9
LIZEMIN <1194538930@qq.com>
2019-12-10 19:02:27 +0300

    Task1.2 was changed

--- Tasks/Task1.2/main.c
+++ Tasks/Task1.2/main.c
@@ -3,7 +3,7 @@
 
 int check_numbers(int x, int y, int z)
 {
-    if (x*x + y*y == z*z)
+    if (x * x + y * y == z * z)
     {
         return 1;
     }
@@ -39,7 +39,6 @@ int check_three_prime(int x, int y, int z)
     return 1;
 }
 
-
 int main()
 {
     setbuf(stdout, NULL);
@@ -47,21 +46,34 @@ int main()
     int x, y, z;
 
     printf("Input x y z: ");
-    scanf("%d %d %d", &x, &y, &z);
 
-    printf("%d %d %d\n", x, y, z);
-
-    if (check_numbers(x, y, z))
+    if (scanf("%d %d %d", &x, &y, &z) != 3)
     {
-        printf("True\n");
+        printf("Incorrect values\n\n");
+        printf("Input x y z: ");
+        setbuf(stdin, NULL);
+        scanf("%d %d %d", &x, &y, &z);
+    }
+
+    if (x < 1 || y < 1 || z < 1)
+    {
+        printf("One or more numbers are not natural\n\n");
+        printf("Input x y z: ");
+        setbuf(stdin, NULL);
+        scanf("%d %d %d", &x, &y, &z);
+    }
+
+    if (check_numbers(x, y, z) || check_numbers(z, y, x) || check_numbers(z, x, y))
+    {
+        printf("Numbers are Pifagor triple\n");
         if (check_three_prime(x, y, z))
-            printf("Numbers are prime");
+            printf("Pifagor triple is primitive\n");
         else
-            printf("Numbers are not prime");
+            printf("Pifagor triple is not primitive\n");
     }
     else
     {
-        printf("False\n");
+        printf("Numbers aren't Pifagor triple\n");
     }
 
     return 0;
