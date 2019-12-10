dc83da5b41c64937d8a63d9e3e24fedf47ee9f6e
LIZEMIN <1194538930@qq.com>
2019-12-10 22:38:50 +0300

    Task1.3 was changed

--- Tasks/Task1.3/main.c
+++ Tasks/Task1.3/main.c
@@ -1,11 +1,3 @@
-//
-//  main.c
-//  task1.3_Углы треугольника
-//
-//  Created by 李泽民 on 15.10.2019.
-//  Copyright © 2019 Ли Цзэминь. All rights reserved.
-//
-
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
@@ -14,14 +6,14 @@
 
 int check_triangular(float a, float b, float c)
 {
-    if ( (a + b > c) && (a + c > b) && (b + c > a) )
+    if ((a + b > c) && (a + c > b) && (b + c > a))
         return 1;
     return 0;
 }
 
 float find_angle(float a, float b, float c)
 {
-    float angle = acos((a*a + b*b - c*c) / (2 * a * b));
+    float angle = acos((a * a + b * b - c * c) / (2 * a * b));
 
     angle = 180 * angle / PI;
 
@@ -30,15 +22,25 @@ float find_angle(float a, float b, float c)
 
 void angle_min_sec(float angle)
 {
-    int grad = angle;
+    int minutes = ((int)(angle * 60)) % 60;
+    int seconds = ((int)(angle * 360)) % 60;
 
-    float frac_part = angle - grad;
+    printf("Grad: %-5d Minutes: %-5d Seconds: %-5d\n", (int) angle, minutes, seconds);
+}
 
-    float minutes = frac_part * 60;
-
-    float seconds = (minutes - (int) minutes) * 60;
-
-    printf("Grad: %-5d Minutes: %-5d Seconds: %-5d\n", grad, (int) round(minutes), (int) round(seconds));
+void input(float* a, float* b, float* c)
+{
+    while (1)
+    {
+        printf("Input a, b, c: ");
+        if (scanf("%f %f %f", a, b, c) != 3)
+        {
+            printf("Incorrect values\n\n");
+            fflush(stdin);
+            continue;
+        }
+        break;
+    }
 }
 
 int main()
@@ -47,9 +49,7 @@ int main()
 
     float a, b, c;
 
-    printf("Input a, b, c: ");
-    scanf("%f %f %f", &a, &b, &c);
-
+    input(&a, &b, &c);
 
     if (!check_triangular(a, b, c))
     {
@@ -63,19 +63,10 @@ int main()
     float second_angle = find_angle(b, c, a);
     float third_angle = find_angle(a, c, b);
 
-
     angle_min_sec(first_angle);
     angle_min_sec(second_angle);
     angle_min_sec(third_angle);
 
     return 0;
 }
-
-// a + b > c
-// a + c > b
-// b + c > a
-
-// 1 / 3 = 0.(3)
-// 2 / 3 = 0.(6)
-
 
