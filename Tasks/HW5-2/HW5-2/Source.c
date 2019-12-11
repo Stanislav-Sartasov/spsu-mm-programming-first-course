17a9757ae4e88b53772b6baef7d56aea6b396127
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2019-12-11 14:56:02 +0300

    Add files via upload

--- Tasks/HW5-2/HW5-2/Source.c
+++ Tasks/HW5-2/HW5-2/Source.c
@@ -4,23 +4,23 @@
 int main()
 {
 
-	int* MDRS = (int*)malloc(sizeof(int) * 1000000);
-	int sum = 0;
+	int* Mdrs = (int*)malloc(sizeof(int) * 1000000);
+	int Sum = 0;
 	for (int i = 2; i < 1000000; i++)
-		MDRS[i] = (i - 1) % 9 + 1;
+		Mdrs[i] = (i - 1) % 9 + 1;
 	for (int i = 2; i < 1000000; i++)
 	{
 		int j = 1;
-		sum += MDRS[i];
+		Sum += Mdrs[i];
 		while (j * i < 1000000)
 		{
-			if (MDRS[i * j] < MDRS[i] + MDRS[j])
-				MDRS[i * j] = MDRS[i] + MDRS[j];
+			if (Mdrs[i * j] < Mdrs[i] + Mdrs[j])
+				Mdrs[i * j] = Mdrs[i] + Mdrs[j];
 			j++;
 		}
 	}
-	free(MDRS);
-	printf("Sum of all MDRS is %d\n", sum);
+	free(Mdrs);
+	printf("Sum of all Mdrs is %d\n", Sum);
 
 	return 0;
 }
