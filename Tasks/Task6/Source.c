b8c48e17a59b7597045742829f1741a677a382ed
David <bykov-david@mail.ru>
2019-12-12 20:48:25 +0300

    - Переработан алгоритм решения

--- Tasks/Task6/Source.c
+++ Tasks/Task6/Source.c
@@ -10,7 +10,16 @@
 
 int cmp(const void* p1, const void* p2)
 {
-	return strcmp(*(char**)p1, *(char**)p2);
+	char* s1 = *(char**)p1;
+	char* s2 = *(char**)p2;
+	for (int i = 0; s1[i] != '\r' && s2[i] != '\r' && s1[i] != '\n' && s2[i] != '\n'; i++)
+	{		
+		if (s1[i] < s2[i])
+			return -1;
+		if (s1[i] > s2[i])
+			return 1;
+	}
+	return 0;
 }
 
 void checkInput(int argc, char* argv[], int fileIn, int fileOut)
@@ -20,7 +29,7 @@ void checkInput(int argc, char* argv[], int fileIn, int fileOut)
 		printf("Check the number of input parameter");
 		exit(-1);
 	}
-	else if ((fileIn = open(argv[1], O_RDONLY)) == -1)
+	else if ((fileIn = open(argv[1], O_RDWR)) == -1)
 	{
 		printf("Input file failed to open");
 		exit(-1);
@@ -32,18 +41,17 @@ void checkInput(int argc, char* argv[], int fileIn, int fileOut)
 	}
 }
 
-void print(int fileOut, char* string, int size)
+int lengthString(char* string)
 {
-	char endl = '\n\0';
-	for (int i = 0; i < size; i++)
-		write(fileOut, &string[i], sizeof(char));
-	write(fileOut, &endl, sizeof(char));
+	int result = 1;
+	for (int i = 2; string[i] != '\n'; i++)
+		result++;
+	return result;
 }
 
 int main(int argc, char* argv[])
 {
 	int fileIn, fileOut;
-
 	checkInput(argc, argv, &fileIn, &fileOut);
 
 	fileIn = open(argv[1], O_RDWR);
@@ -60,48 +68,34 @@ int main(int argc, char* argv[])
 	}
 
 	int countLines = 0;
-	int maxLengthofString = 0;
-	int currentCounter = 0;
 
 	for (int i = 0; i < stat.st_size / sizeof(char); i++)
 		if (map[i] == '\n')
-		{
 			countLines++;
-			maxLengthofString = i - currentCounter - 1 > maxLengthofString ? i - currentCounter - 1 : maxLengthofString;
-			currentCounter = i + 1;
-		}
 
-	char** strings = (char**)malloc(countLines * sizeof(char*));
-	int* lengthOfStrings = (int*)malloc(countLines * sizeof(int));
+	char** strings = (char**)malloc((countLines + 1)  * sizeof(char*));
 
-	for (int i = 0, counter = 0, previousCounter = 0; i < stat.st_size / sizeof(char);)
+	strings[0] = &map[0];
+	for (int i = 0, counter = 1; i < stat.st_size / sizeof(char);)
 	{
-
 		while (map[i] != '\n' && map[i] != '\r')
 			i += 1;
 
-		if (counter == 0)
-			lengthOfStrings[counter] = i;
-		else
-			lengthOfStrings[counter] = i - previousCounter;
-
-		previousCounter += lengthOfStrings[counter] + 2;
-
-		char* currentLine = (char*)malloc(lengthOfStrings[counter] * sizeof(char));
-
-		snprintf(currentLine, lengthOfStrings[counter] + 1, &map[i - lengthOfStrings[counter]]);
-		strings[counter] = currentLine;
+		strings[counter] = &map[i + 2];
 
 		if (map[i] == '\r')
 			i++;
 		i++;
 		counter++;
 	}
-
 	qsort(strings, countLines, sizeof(char*), cmp);
 
+	char endl = '\n';
 	for (int i = 0; i < countLines; i++)
-		print(fileOut, strings[i], strlen(strings[i]));
+	{
+		write(fileOut, strings[i], lengthString(strings[i]));
+		write(fileOut, &endl, sizeof(char));
+	}
 
 	free(strings);
 	munmap(map, stat.st_size);
