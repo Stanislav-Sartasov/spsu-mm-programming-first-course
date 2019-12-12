bc44c70c5e5df149255eeacfbffd85ba4de301cf
Krekep <pashaalimov@gmail.com>
2019-12-12 19:09:05 +0300

    - Исправлена программа 6

--- Tasks/String_sort/main.c
+++ Tasks/String_sort/main.c
@@ -9,16 +9,18 @@ int comparator(const void* a, const void* b)
 	const char* s1, * s2;
 	s1 = *(char**)a;
 	s2 = *(char**)b;
-	int i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\n' && s2[i] != '\n')
-		i++;
-	return (s1[i] - s2[i]);
+	return strcmp(s1, s2);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	int file_in = _open("input.txt", O_RDWR, 0);
-	FILE* file_out = fopen("output.txt", "w");
+    if (argc != 3)
+	{
+		printf("Invalid number of parameters");
+		return 1;
+	}
+	int file_in = _open(argv[1], O_RDWR, 0);
+	FILE* file_out = fopen(argv[2], "w");
 	if (file_in == -1 || file_out == NULL)
 	{
 		printf("Opening error\n");
@@ -34,7 +36,7 @@ int main()
 		return 1;
 	}
 	int len_input_file = strlen(text);
-	int count_of_string = 1;
+	int count_of_string = 0;
 	for (int i = 0; i < len_input_file; i++)
 		if (text[i] == '\n')
 			count_of_string++;
@@ -66,19 +68,20 @@ int main()
 	for (i = 0; i < count_of_string; i++)
 	{
 		int pos = 0;
-		while (strings[i][pos] != '\0')
-		{
-			fputc(strings[i][pos], file_out);
-			pos++;
-		}
+		if (strings[i])
+            while (strings[i][pos] != '\0')
+            {
+                fputc(strings[i][pos], file_out);
+                pos++;
+            }
 		fputc('\n', file_out);
 	}
-	fputc('\0', file_out);
 	munmap(text, len_input_file);
 	for (i = 0; i < count_of_string; i++)
 		free(strings[i]);
 	free(strings);
 	_close(file_in);
 	fclose(file_out);
+	printf("Success\n");
 	return 0;
 }
