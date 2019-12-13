1353c18fcab3131cfe193cfb88289c6706eaa327
makar-pelogeiko <m.pelogeiko@mail.ru>
2019-12-14 01:16:13 +0300

    задача "цепные дроби" - исправлены пункты кодстайла 7, 11 - добавлен вывод периода

--- dz_september/chain_fraction/main.c
+++ dz_september/chain_fraction/main.c
@@ -4,31 +4,34 @@
 
 int main()
 {
-    long long q, q0, denum =0, number = 0;
-    //double a = saveIn();
-    long long a = saveInInt();
+    long long q, q0, denum = 0, number = 0, a, period = 0;
+
+    printf("This programm give's elements of continious fraction for entred sqrt(x)\n");
+    do
+    {
+        printf("enter yor number(not full square of number): ");
+        a = saveInInt();
+    }
+    while (sqrt(a) - trunc(sqrt(a)) == 0);
 
     q = (long long)trunc(sqrt(a));
-
-    if ((long long)a - q * q == 0)
-    {
-        printf("%lld", q);
-        return 0;
-    }
 
     printf("%lld ", q);
     q0 = (long long)(q + q) % (long long)(a - q * q) - q;
     denum = (long long)a - q * q;
 
-    number = (-q0 + q) / denum;
+    ++period;
+    number = (- q0 + q) / denum;
     printf("%lld ", number);
 
     for (;2 * q != number;)
     {
         denum = (long long)(a - q0 * q0) / (long long)(denum);
-        number = (-q0 + q) / denum;
+        number = (- q0 + q) / denum;
         printf("%lld ", number);
+        ++period;
         q0 = (long long)(q - q0) % (long long)(denum) - q;
     }
+    printf("\nperiod is: %lld", period);
     return 0;
 }
