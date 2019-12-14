fee7f1ca4b1ed4d144ebf27ab5a417989d75340e
Lyubov Miloserdova <55358539+miloserdova-l@users.noreply.github.com>
2019-12-14 20:34:34 +0000

    - добавлено решение задачи №9

--- task9/allocators.c
+++ task9/allocators.c
@@ -10,9 +10,9 @@ int has_initialized = 0;
 
 void init()
 {
-	last_valid_address = (void*) malloc(memory_size);
-	managed_memory_start = last_valid_address;
- 	has_initialized = 1;
+    last_valid_address = (void*) malloc(memory_size);
+    managed_memory_start = last_valid_address;
+    has_initialized = 1;
 }
 
 void uninit(void *ptr)
@@ -22,58 +22,58 @@ void uninit(void *ptr)
 
 void* myMalloc(size_t size)
 {
-	void *current_location;
-	mem_control_block *current_location_mcb;
-	void *memory_location;
-	if (!has_initialized)
-		init();
-	size = size + sizeof(mem_control_block);
-	memory_location = 0;
-	current_location = managed_memory_start;
+    void *current_location;
+    mem_control_block *current_location_mcb;
+    void *memory_location;
+    if (!has_initialized)
+        init();
+    size = size + sizeof(mem_control_block);
+    memory_location = 0;
+    current_location = managed_memory_start;
 
-	while(current_location != last_valid_address)
-	{
+    while(current_location != last_valid_address)
+    {
         current_location_mcb = (mem_control_block*) current_location;
-		if(current_location_mcb->is_available)
-		{
-			if(current_location_mcb->size >= size)
-			{
-				current_location_mcb->is_available = 0;
-				memory_location = current_location;
-				break;
-			}
-		}
-		current_location = current_location + current_location_mcb->size;
-	}
+        if(current_location_mcb->is_available)
+        {
+            if(current_location_mcb->size >= size)
+            {
+                current_location_mcb->is_available = 0;
+                memory_location = current_location;
+                break;
+            }
+        }
+        current_location = current_location + current_location_mcb->size;
+    }
 
-	if(!memory_location)
-	{
-		init();
-		memory_location = last_valid_address;
-		last_valid_address = last_valid_address + size;
+    if(!memory_location)
+    {
+        init();
+        memory_location = last_valid_address;
+        last_valid_address = last_valid_address + size;
 
-		current_location_mcb = memory_location;
-		current_location_mcb->is_available = 0;
-		current_location_mcb->size = size;
-	}
+        current_location_mcb = memory_location;
+        current_location_mcb->is_available = 0;
+        current_location_mcb->size = size;
+    }
 
-	memory_location = memory_location + sizeof(mem_control_block);
-	return memory_location;
+    memory_location = memory_location + sizeof(mem_control_block);
+    return memory_location;
 }
 
 void myFree(void* ptr)
 {
-	mem_control_block *mcb;
-	mcb = ptr - sizeof(mem_control_block);
-	mcb->is_available = 1;
-	uninit(ptr);
-	return;
+    mem_control_block *mcb;
+    mcb = ptr - sizeof(mem_control_block);
+    mcb->is_available = 1;
+    uninit(ptr);
+    return;
 }
 
 void* myRealloc(void* ptr, size_t size)
 {
-	void *memory_location;
-	memory_location = myMalloc(size);
+    void *memory_location;
+    memory_location = myMalloc(size);
     memset(memory_location, ptr, size + sizeof(mem_control_block));
     myFree(ptr);
     return memory_location;
