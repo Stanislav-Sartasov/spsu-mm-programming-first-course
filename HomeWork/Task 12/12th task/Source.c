d112e97b050861492324a571c359df57d08bacaa
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2019-12-14 12:24:34 +0300

    Update Source.c

--- HomeWork/Task 12/12th task/Source.c
+++ HomeWork/Task 12/12th task/Source.c
@@ -1,14 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void myPow(int *mas, int n, int pow)
+void myPow(int *mas, int n, int pow, int quantity)
 {
 	int i, j;
 	for (i = 0; i < pow; i++)
 	{
-		for (j = 0; j < i; j++) mas[j] *= n;
-		for (j = 0; j < i; j++)
+		for (j = 0; j < quantity; j++) mas[j] *= n;
+		for (j = 0; j < quantity; j++)
 			if (mas[j] >= 16)
 			{
 				mas[j + 1] += mas[j] / 16;
@@ -16,36 +17,19 @@ void myPow(int *mas, int n, int pow)
 			}
 	}
 
-	for (j = 0; j < pow; j++) mas[j] *= n;
-	for (j = 0; j < pow - 1; j++)
-		if (mas[j] >= 16)
-		{
-			mas[j + 1] += mas[j] / 16;
-			mas[j] %= 16;
-		}
 }
 
 int main()
 {
-	int n = 5000, i;
+	int n, i;
+	n = (5000 * (log(3) / log(16)) + 1);
 	int *mas = (int*)malloc(n * sizeof(int));
 	mas[0] = 1;
 	for (i = 1; i < n; i++) mas[i] = 0;
 
-	myPow(mas, 3, n);
+	myPow(mas, 3, 5000, n);
 
-
-	int zero = 0;
-	for (i = n - 1; i >= 0; i--)
-		if (mas[i] != 0)
-		{
-			zero = i + 1;
-			break;
-		}
-
-	mas = realloc(mas, zero * sizeof(int));
-
-	for (i = zero - 1; i >= 0; i--) printf("%x", mas[i]);
+	for (i = n - 1; i >=0; i--) printf("%x", mas[i]);
 	printf("\n");
 	free(mas);
 	system("pause");
