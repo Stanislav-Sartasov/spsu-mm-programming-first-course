ea05a77b42fbb9fee4e14da3ec4aee90686e6c41
LIZEMIN <1194538930@qq.com>
2019-12-14 11:24:16 +0300

    Task1.5 was changed

--- Tasks/Task1.5/main.c
+++ Tasks/Task1.5/main.c
@@ -3,52 +3,51 @@
 
 int is_sqrt(int number)
 {
-    for (int i = 1; i*i <= number; i++)
-    {
-        if (i * i == number)
-            return 0;
-    }
-    return 1;
+	for (int i = 1; i*i <= number; i++)
+	{
+		if (i * i == number)
+			return 0;
+	}
+	return 1;
 }
 
-void get_fraction(int number)
+int get_fraction(int number)
 {
-    float frac_elem = sqrt(number);
-    int int_part = frac_elem;
+	float frac_elem = sqrt(number);
+	int int_part = frac_elem;
 
-    printf("%d ", int_part);
+	printf("%d ", int_part);
 
-    float next_elem = frac_elem;
-    int next_int_part = int_part;
+	float next_elem = frac_elem;
+	int next_int_part = int_part;
 
-    int period = 0;
-    while (2 * int_part != next_int_part)
-    {
-        next_elem = 1 / (next_elem - next_int_part);
-        next_int_part = next_elem;
+	int period = 0;
+	while (2 * int_part != next_int_part)
+	{
+		next_elem = 1 / (next_elem - next_int_part);
+		next_int_part = next_elem;
 
-        printf("%d ", next_int_part);
-        period++;
-    }
+		printf("%d ", next_int_part);
+		period++;
+	}
 
-    printf("\nPeriod length: %d\n", period);
+	printf("\nPeriod length: %d\n", period);
 }
 
 int main()
 {
-    int number;
+	int number;
 
-    printf("Input number: ");
-    scanf("%d", &number);
+	printf("Input number: ");
+	scanf("%d", &number);
 
-    if (!is_sqrt(number))
-    {
-        printf("Incorrect number\n");
-        return 0;
-    }
+	if (!is_sqrt(number))
+	{
+		printf("Incorrect number\n");
+		return 0;
+	}
 
-    get_fraction(number);
+	get_fraction(number);
 
-    return 0;
+	return 0;
 }
-
