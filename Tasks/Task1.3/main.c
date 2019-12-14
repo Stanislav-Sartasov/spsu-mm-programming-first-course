e02e50a313092f10033cf3e070502253a3f42ef4
LIZEMIN <1194538930@qq.com>
2019-12-14 17:15:21 +0300

    Task1.3 was changed

--- Tasks/Task1.3/main.c
+++ Tasks/Task1.3/main.c
@@ -6,75 +6,75 @@
 
 int check_triangular(float a, float b, float c)
 {
-    if ((a + b > c) && (a + c > b) && (b + c > a))
-        return 1;
-    return 0;
+	if ((a + b > c) && (a + c > b) && (b + c > a))
+		return 1;
+	return 0;
 }
 
 float find_angle(float a, float b, float c)
 {
-    float angle = acos((a * a + b * b - c * c) / (2 * a * b));
+	float angle = acos((a * a + b * b - c * c) / (2 * a * b));
 
-    angle = 180 * angle / PI;
+	angle = 180 * angle / PI;
 
-    return angle;
+	return angle;
 }
 
 void angle_min_sec(float angle)
 {
-    int minutes = ((int)(angle * 60)) % 60;
-    int seconds = ((int)(angle * 360)) % 60;
+	int minutes = ((int)(angle * 60)) % 60;
+	int seconds = ((int)(angle * 360)) % 60;
 
-    printf("Grad: %-5d Minutes: %-5d Seconds: %-5d\n", (int) angle, minutes, seconds);
+	printf("Grad: %-5d Minutes: %-5d Seconds: %-5d\n", (int) angle, minutes, seconds);
 }
 
 void input(float* a, float* b, float* c)
 {
-    while (1)
-    {
-        printf("Input a, b, c: ");
-        if (scanf("%f %f %f", a, b, c) != 3)
-        {
-            printf("Incorrect values\n\n");
-            int symb;
-            while ((symb = getchar()) != '\n' && symb != EOF);
-            continue;
-        }
+	while (1)
+	{
+		printf("Input a, b, c: ");
+		if (scanf("%f %f %f", a, b, c) != 3)
+		{
+			printf("Incorrect values\n\n");
+			int symb;
+			while ((symb = getchar()) != '\n' && symb != EOF);
+			continue;
+		}
 
-        if (*a <= 0 || *b <= 0 || *c <= 0)
-        {
-            printf("Incorrect length\n\n");
-            int symb;
-            while ((symb = getchar()) != '\n' && symb != EOF);
-            continue;
-        }
-        break;
-    }
+		if (*a <= 0 || *b <= 0 || *c <= 0)
+		{
+			printf("Incorrect length\n\n");
+			int symb;
+			while ((symb = getchar()) != '\n' && symb != EOF);
+			continue;
+		}
+		break;
+	}
 }
 
 int main()
 {
-    setbuf(stdout, NULL);
+	setbuf(stdout, NULL);
 
-    float a, b, c;
+	float a, b, c;
 
-    input(&a, &b, &c);
+	input(&a, &b, &c);
 
-    if (!check_triangular(a, b, c))
-    {
-        printf("Triangular does not exist\n");
-        return 0;
-    }
+	if (!check_triangular(a, b, c))
+	{
+		printf("Triangular does not exist\n");
+		return 0;
+	}
 
-    printf("Triangular exists\n");
+	printf("Triangular exists\n");
 
-    float first_angle = find_angle(a, b, c);
-    float second_angle = find_angle(b, c, a);
-    float third_angle = find_angle(a, c, b);
+	float first_angle = find_angle(a, b, c);
+	float second_angle = find_angle(b, c, a);
+	float third_angle = find_angle(a, c, b);
 
-    angle_min_sec(first_angle);
-    angle_min_sec(second_angle);
-    angle_min_sec(third_angle);
+	angle_min_sec(first_angle);
+	angle_min_sec(second_angle);
+	angle_min_sec(third_angle);
 
-    return 0;
+	return 0;
 }
