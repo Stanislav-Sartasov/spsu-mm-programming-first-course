6ca2e0b757c32d11f790844a864850a508e88b8d
LIZEMIN <1194538930@qq.com>
2019-12-14 17:16:55 +0300

    Task1.4 was changed

--- Tasks/Task1.4/main.c
+++ Tasks/Task1.4/main.c
@@ -3,30 +3,30 @@
 
 int is_prime(long int n)
 {
-    for (int i = 2; i < ((int) sqrt(n - 1)) + 1; i++)
-    {
-        if (n % i == 0)
-            return 0;
-    }
-    return 1;
+	for (int i = 2; i < ((int) sqrt(n - 1)) + 1; i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
 }
 
 int main()
 {
-    int n_start = 2;
-    long int curr_mersenne = 3;
+	int n_start = 2;
+	long int curr_mersenne = 3;
 
-    long int end = pow(2, 32) - 1;
+	long int end = pow(2, 32) - 1;
 
-    printf("Prime Mersenne numbers:\n");
-    while (curr_mersenne <= end)
-    {
-        if (is_prime(curr_mersenne))
-            printf("%ld\n", curr_mersenne);
-        n_start++;
+	printf("Prime Mersenne numbers:\n");
+	while (curr_mersenne <= end)
+	{
+		if (is_prime(curr_mersenne))
+			printf("%ld\n", curr_mersenne);
+		n_start++;
 
-        curr_mersenne = pow(2, n_start) - 1;
-    }
+		curr_mersenne = pow(2, n_start) - 1;
+	}
 
-    return 0;
+	return 0;
 }
