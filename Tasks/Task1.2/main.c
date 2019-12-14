6494223c682fc9ef273e3ea531bb461c14798b6e
LIZEMIN <1194538930@qq.com>
2019-12-14 17:13:11 +0300

    Task1.2 was changed

--- Tasks/Task1.2/main.c
+++ Tasks/Task1.2/main.c
@@ -3,69 +3,69 @@
 
 int check_numbers(int x, int y, int z)
 {
-    if (x * x + y * y == z * z)
-    {
-        return 1;
-    }
-    return 0;
+	if (x * x + y * y == z * z)
+	{
+		return 1;
+	}
+	return 0;
 }
 
 int gcd(int a, int b)
 {
-    int buf;
-    while (b > 0)
-    {
-        buf = a % b;
-        a = b;
-        b = buf;
-    }
-    return a;
+	int buf;
+	while (b > 0)
+	{
+		buf = a % b;
+		a = b;
+		b = buf;
+	}
+	return a;
 }
 
 void input(int* x, int* y, int* z)
 {
-    while (1)
-    {
-        printf("Input x y z: ");
-        if (scanf("%d %d %d", x, y, z) != 3)
-        {
-            printf("Incorrect values\n\n");
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF);
-            continue;
-        }
+	while (1)
+	{
+		printf("Input x y z: ");
+		if (scanf("%d %d %d", x, y, z) != 3)
+		{
+			printf("Incorrect values\n\n");
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF);
+			continue;
+		}
 
-        if (*x < 1 || *y < 1 || *z < 1)
-        {
-            printf("One or more numbers are not natural\n\n");
-            int c;
-            while ((c = getchar()) != '\n' && c != EOF);
-            continue;
-        }
-        break;
-    }
+		if (*x < 1 || *y < 1 || *z < 1)
+		{
+			printf("One or more numbers are not natural\n\n");
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF);
+			continue;
+		}
+		break;
+	}
 }
 
 int main()
 {
-    setbuf(stdout, NULL);
+	setbuf(stdout, NULL);
 
-    int x, y, z;
+	int x, y, z;
 
-    input(&x, &y, &z);
+	input(&x, &y, &z);
 
-    if (check_numbers(x, y, z) || check_numbers(z, y, x) || check_numbers(z, x, y))
-    {
-        printf("Numbers are Pifagor triple\n");
-        if (gcd(gcd(x, y), z) == 1)
-            printf("Pifagor triple is primitive\n");
-        else
-            printf("Pifagor triple is not primitive\n");
-    }
-    else
-    {
-        printf("Numbers aren't Pifagor triple\n");
-    }
+	if (check_numbers(x, y, z) || check_numbers(z, y, x) || check_numbers(z, x, y))
+	{
+		printf("Numbers are Pifagor triple\n");
+		if (gcd(gcd(x, y), z) == 1)
+			printf("Pifagor triple is primitive\n");
+		else
+			printf("Pifagor triple is not primitive\n");
+	}
+	else
+	{
+		printf("Numbers aren't Pifagor triple\n");
+	}
 
-    return 0;
+	return 0;
 }
