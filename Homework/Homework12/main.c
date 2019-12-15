14102cec0f2e7681daad8d87880f3269df59689b
Илья <i7ok@yandex.ru>
2019-12-15 18:31:35 +0300

    Homework12

---
+++ Homework/Homework12/main.c
@@ -0,0 +1,24 @@
+#include "gmp.h"
+#include <stdio.h>
+#include <string.h>
+
+// Please change these constants to test the other numbers:
+#define BASE 3
+#define EXPONENT 5000
+
+
+// The main program starts here.
+int main()
+{
+	struct MP_INT* a = (struct MP_INT*)malloc(sizeof(struct MP_INT*));
+	mpz_init_set_ui(a, BASE);
+	mpz_pow_ui(a, a, EXPONENT); 
+
+	char* s = mpz_get_str(0, 16, a);
+	printf("Digits: %s\n", s);
+
+
+	free(s);
+
+	return 0;
+} 
