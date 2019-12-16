c1f4292175e99182fcf26ac4987bacc8bf150939
ToxaKaz <anton.a.kazancev@gmail.com>
2019-12-16 10:38:56 +0300

    Make up

--- Solutions/Task_6/Task_6.c
+++ Solutions/Task_6/Task_6.c
@@ -54,8 +54,12 @@ int compare(const void** arg_1, const void** arg_2)
 {
 	char* in_1 = (char*)(*arg_1);
 	char* in_2 = (char*)(*arg_2);
-	for (int i = 0; in_1[i] != '\0' || in_2[i] != '\0'; i++)
-		if (in_1[i] > in_2[i])
+	for (int i = 0; (in_1[i] != '\0' && in_1[i] != '\n' && in_1[i] != '\r') || (in_2[i] != '\0' && in_2[i] != '\n' && in_2[i] != '\r'); i++)
+		if ((in_1[i] == '\0' || in_1[i] == '\n' || in_1[i] == '\r') && (in_2[i] != '\0' && in_2[i] != '\n' && in_2[i] != '\r'))
+			return 1;
+		else if ((in_2[i] == '\0' || in_2[i] == '\n' || in_2[i] == '\r') && (in_1[i] != '\0' && in_1[i] != '\n' && in_1[i] != '\r'))
+			return -1;
+		else if (in_1[i] > in_2[i])
 			return 1;
 		else if (in_1[i] < in_2[i])
 			return -1;
@@ -70,29 +74,40 @@ int main(int argc, char** argv)
 
 	if (argc == 1)
 	{
-		for (int i = 2;; i++)
+		printf("\tinputing format: <input_file_name> <output_file_name>\n > ");
+		for (;;)
 		{
-			str[i] = getchar();
-			if (str[i] == '\n')
+			for (int i = 2;; i++)
 			{
-				str[i] = '\0';
-				break;
+				str[i] = getchar();
+				if (str[i] == '\n')
+				{
+					str[i] = '\0';
+					break;
+				}
+				str = (char*)realloc(str, sizeof(char) * (i + 2));
 			}
-			str = (char*)realloc(str, sizeof(char) * (i + 2));
+			argv = strgs(&str, &argc);
+			if (argc != 3)
+			{
+				printf(" > invalid input\n > ");
+				str[1] = ' ';
+			}
+			else
+				break;
 		}
-		argv = strgs(&str, &argc);
 	}
 
 	if (argc != 3)
 	{
-		printf("invalid input");
+		printf(" > invalid input\n\tinputing format: <input_file_name> <output_file_name>");
 		return -1;
 	}
 
 	int file_in = _open(argv[1], O_RDWR, 0);
 	if (file_in == -1)
 	{
-		printf("invalid inputing file name");
+		printf(" > invalid inputing file name");
 		return -1;
 	}
 	struct stat file_in_stat;
@@ -102,7 +117,7 @@ int main(int argc, char** argv)
 	char* file_in_mmap = mmap(0, file_in_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, file_in, 0);
 	if (file_in_mmap == MAP_FAILED)
 	{
-		printf("mmap fail");
+		printf(" > mmap fail");
 		_close(file_in);
 		return -1;
 	}
@@ -124,32 +139,19 @@ int main(int argc, char** argv)
 
 	for (int i = 0; i < file_in_size; i++)
 		if (file_in_mmap[i] == '\n')
-		{
-			file_in_mmap[i] = '\0';
 			arr_str[arr_size++] = file_in_mmap + i + 1;
-		}
 		else if (file_in_mmap[i] == '\r')
-		{
-			file_in_mmap[i] = '\0';
-			++i;
-			arr_str[arr_size++] = file_in_mmap + i + 1;
-		}
+			arr_str[arr_size++] = file_in_mmap + ++i + 1;
 
 	qsort(arr_str, arr_size, sizeof(char*), compare);
 
 	for (int i = 0; i < arr_size; i++)
 	{
-		fprintf(file_out, "%s", arr_str[i]);
-		if (i + 1 < arr_size)
+		for (int j = 0; arr_str[i][j] != '\0' && arr_str[i][j] != '\n' && arr_str[i][j] != '\r'; j++)
+			fprintf(file_out, "%c", arr_str[i][j]);
+		if (i < arr_size - 1)
 			fprintf(file_out, "\n");
 	}
-
-	for (int i = 0; i < arr_size; i++)
-		if (arr_str[i] - 1 > file_in_mmap)
-			if (*(arr_str[i] - 1) == '\n')
-				*(arr_str[i] - 2) = '\r';
-			else
-				*(arr_str[i] - 1) = '\n';
 
 	munmap(file_in_mmap, file_in_size);
 
