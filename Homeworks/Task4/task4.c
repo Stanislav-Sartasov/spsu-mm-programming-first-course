1e9da395affdf2954f51e16063b70304bfbfd998
Yunus Emre <comtcyber@gmail.com>
2019-12-17 14:45:52 +0300

    Task4, problem fixed

--- Homeworks/Task4/task4.c
+++ Homeworks/Task4/task4.c
@@ -9,7 +9,7 @@ int is_prime_number(unsigned int num)
 		return 0;
 	}
 
-	for (i = 2 ; i <= num / 2 ; i++)
+	for (i = 2 ; i * i <= num ; i++)
 	{
 		if ((num % i) == 0)
 		{
@@ -25,19 +25,15 @@ int main()
 	int i, j;
 	unsigned int number;
 
+	number = 1;
 	for (i = 1 ; i < 32 ; i++)
 	{
-		number = 1;
-		for (j = 0 ; j < i ; j++)
-		{
-			number = number * 2;
-		}
-		number = number - 1;
-
 		if (is_prime_number(number))
 		{
 			printf("Mersenne number(%d) is %d\n", i, number);
 		}
+
+		number = 2 * number + 1;
 	}
 
 	return 0;
