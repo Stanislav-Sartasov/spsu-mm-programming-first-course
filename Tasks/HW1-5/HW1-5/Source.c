4f6f0bd79a817f801dc555703b1beb7f6f1117b9
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2019-12-18 15:03:21 +0300

    Add files via upload

--- Tasks/HW1-5/HW1-5/Source.c
+++ Tasks/HW1-5/HW1-5/Source.c
@@ -1,51 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
-#include <stdlib.h>
+
+int is_square(int n)
+{
+	for (int i = (int)sqrt(n) - 1; i * i <= (n + 1); i++)
+		if (i * i == n)
+			return 0;
+	return 1;
+}
 
 int main()
 {
-	printf("Enter the irrational number from which you want to take the root\n");
-	int period = 0;
-	double main_num;
+	int n;
 
-	while (!scanf("%lf", &main_num) )
+	printf("Enter the natural number whose continued fraction you want to get\n");
+	while ((!scanf_s("%d", &n)) || (is_square(n) == 0))
 	{
 		while (getchar() != '\n');
-		printf("This is not a irrational number\ntry again: ");
+		printf("Input Error\nEnter not square natural number\ntry again: ");
 	}
 
+	double x = sqrt(n);
+	double m = 0, s = 1, a = (int)sqrt(n);
 
-	if (sqrt(main_num) == (int)sqrt(main_num)) 
+	printf("Continued fraction is: [%.0f; ", a);
+
+	while (1)
 	{
-		printf("Number is rational, try again\n");
-		return main();
-	}
-
-	main_num = sqrt(main_num);
-
-	int whole = main_num;
-	double divider = pow(main_num - whole, -1);
-
-	double a = main_num;
-
-	while (1) 
-	{
-		int b = a;
-		if (period == 0)
-			printf("[%d;", b);
+		m = a * s - m;
+		s = (n - m * m) / s;
+		x = (m + sqrt(n)) / s;
+		a = (int)x;
+		if (s != 1 && a != 0)
+			printf("%.0f, ", a);
 		else
-			printf(" %d", b);
-		double c = a - b;
-		a = pow(c, -1);
-		if (((float)a == (float)divider && period != 0) || period == 19)
-		{
-			printf("]\nperiod = %d", period);
 			break;
-		}
-
-		period++;
 	}
+
+	printf("%.0f]\n", a);
+
 	return 0;
 }
