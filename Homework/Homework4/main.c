4bb5ef68770dea05430e62e9f4f2959285856c30
Илья <i7ok@yandex.ru>
2019-12-22 22:38:35 +0300

    Homework4, remake

--- Homework/Homework4/main.c
+++ Homework/Homework4/main.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdint.h>
 
+uint32_t pows[32];
 
-// The main program starts here.
-int main() {
-	printf("All Mersenne primes on the interval [1; 2 ^ 31 - 1]: \n");
+void calculatePow2() 
+{
+    pows[0] = 1;
+    for(int i = 1; i < 32; ++i) 
+	{
+        pows[i] = pows[i - 1] * 2;
+    }
+}
 
-	for (int i = 1; i <= 31; i++) {
-		printf("%d\n", (int)pow(2, i) - 1);
-	}
-	
-	return 0;
+int main() 
+{
+    const char* description = "This program print's all the Mersenne primes in"
+                              " interval [1, 2^31 - 1]\n";
+
+    printf("%s", description);
+
+    calculatePow2();
+
+    int mersenneExp[8] = {2, 3, 5, 7, 13, 17, 19, 31};
+
+    for (int i = 0; i < 8; ++i) 
+	{
+        printf("%d\n", pows[mersenneExp[i]] - 1);
+    }
 }
