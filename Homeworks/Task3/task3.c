200e25fa039b77e689ef7e44980652684e65602d
Yunus Emre <lucifgy@yahoo.com>
2019-12-23 19:04:43 +0300

    Task3, fixed

--- Homeworks/Task3/task3.c
+++ Homeworks/Task3/task3.c
@@ -3,6 +3,18 @@
 #include <stdlib.h>
 #include <math.h>
 
+int compare_sides(const void* a1, const void* a2)
+{
+	int * pSide1 = (int*)a1;
+	int * pSide2 = (int*)a2;
+
+	if (*pSide1 > *pSide2)
+		return 1;
+	if (*pSide1 == *pSide2)
+		return 0;
+	return -1;
+}
+
 double get_angle(double side1, double side2, double side3)
 {
 	double PI = 3.141592;
@@ -16,6 +28,7 @@ int main()
 	char strs[3][1000];
 	int i;
 	double angle[3], degree, minute, second;
+	int sum_degree, sum_minute, sum_second;
 
 	for (;;)
 	{
@@ -25,6 +38,8 @@ int main()
 		sides[0] = atoi(strs[0]);
 		sides[1] = atoi(strs[1]);
 		sides[2] = atoi(strs[2]);
+
+		qsort(sides, 3, sizeof(int), compare_sides);
 
 		if (sides[0] > 0 && sides[1] > 0 && sides[2] > 0)
 		{
@@ -41,19 +56,50 @@ int main()
 
 		angle[0] = get_angle(sides[0], sides[1], sides[2]);
 		angle[1] = get_angle(sides[1], sides[2], sides[0]);
-		angle[2] = ceil(180.0 - angle[0] - angle[1]);
-		for (i = 0; i < 3; i++)
+		angle[2] = 180.0 - angle[0] - angle[1];
+
+		sum_degree = 0;
+		sum_minute = 0;
+		sum_second = 0;
+		for (i = 0; i < 2 ; i++)
 		{
 			minute = modf(angle[i], &degree);
 			second = modf(minute * 60, &minute);
 			second = floor(second * 60);
 			printf("angle(%d): %f degrees %f minutes %f seconds\n", i + 1, degree, minute, second);
+			sum_second += (int)second;
+			if (sum_second >= 60)
+			{
+				sum_second -= 60;
+				sum_minute += 1;
+			}
+			sum_minute += (int)minute;
+			if (sum_minute >= 60)
+			{
+				sum_minute -= 60;
+				sum_degree += 1;
+			}
+			sum_degree += degree;
 		}
+		second = 60 - sum_second;
+		minute = 59 - sum_minute;
+		degree = 179 - sum_degree;
+		if (second >= 60)
+		{
+			second -= 60;
+			minute += 1;
+		}
+		if(minute >= 60)
+		{
+			minute -= 60;
+			degree += 1;
+		}
+		printf("angle(%d): %f degrees %f minutes %f seconds\n", 3, degree, minute, second);
 	}
 	else
 	{
 		printf("The numbers are impossible as triangle's side.\n");
 	}
-	
+
 	return 0;
 }
