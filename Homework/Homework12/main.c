9313f572cfa7e4cc014b06d39a7bed63aa887ea1
Илья <i7ok@yandex.ru>
2019-12-24 17:40:24 +0300

    HWFixed.v4

--- Homework/Homework12/main.c
+++ Homework/Homework12/main.c
@@ -10,116 +10,117 @@ typedef struct
     // ....
     // buffer[n] = stores the first digit
     // each element of buffer is in range [1, 15]
-    uint8_t buffer[MAX_DIGITS];
-} BigInteger;
+	uint8_t buffer[MAX_DIGITS];
+}
+BigInteger;
 
 // increments a digit at index, by inc in BigInteger num.
 void incrementDigit(BigInteger* num, int index, int inc)
 {
-    int carry = inc;
-    while (carry) 
+	int carry = inc;
+	while (carry) 
 	{
-        int v = num->buffer[index] + carry;
-        num->buffer[index++] = (v % 16);
-        carry = (v / 16);
-    }
+		int v = num->buffer[index] + carry;
+		num->buffer[index++] = (v % 16);
+		carry = (v / 16);
+	}
 }
 
 // counts the number of digits in a BigInteger
 int digitCount(BigInteger const* num) 
 {
-    for(int i = MAX_DIGITS - 1; i >= 0; --i) 
+	for(int i = MAX_DIGITS - 1; i >= 0; --i) 
 	{
-        if(num->buffer[i] != 0)
-            return i + 1;
+		if(num->buffer[i] != 0)
+			return i + 1;
     }
-    return 0;
+	return 0;
 }
 
 void print(BigInteger const* num) 
 {
-    char hex[] = "0123456789abcdef";
-    int found = 0;
-    for(int i = MAX_DIGITS - 1; i >= 0; --i) 
+	char hex[] = "0123456789abcdef";
+	int found = 0;
+	for(int i = MAX_DIGITS - 1; i >= 0; --i) 
 	{
-        if (num->buffer[i] == 0 && found == 0)
-            continue;
-        found = 1;
-        printf("%c", hex[num->buffer[i]]);
+		if (num->buffer[i] == 0 && found == 0)
+			continue;
+		found = 1;
+		printf("%c", hex[num->buffer[i]]);
     }
 
-    if (found == 0)
-        printf("0");
+	if (found == 0)
+		printf("0");
 
-    printf("\n");
+	printf("\n");
 }
 
 // create a new BitInteger object, with initial value
 BigInteger create(unsigned int initial)
 {
-    BigInteger res;
-    int index = 0;
-    while(initial) 
+	BigInteger res;
+	int index = 0;
+	while(initial) 
 	{
-        res.buffer[index++] = (initial % 16);
-        initial /= 16;
-    }
-    while (index < MAX_DIGITS)
-        res.buffer[index++] = 0;
-    return res;
+		res.buffer[index++] = (initial % 16);
+		initial /= 16;
+	}
+	while (index < MAX_DIGITS)
+		res.buffer[index++] = 0;
+	return res;
 }
 
 BigInteger multiply(BigInteger const* x, BigInteger const* y)
 {
-    BigInteger res = create(0);
-    int carry = 0;
+	BigInteger res = create(0);
+	int carry = 0;
 
-    int cx = digitCount(x);
-    int cy = digitCount(y);
+	int cx = digitCount(x);
+	int cy = digitCount(y);
 
-    for(int i = 0; i < cy; ++i) 
+	for(int i = 0; i < cy; ++i) 
 	{
-        int index = i;
-        for(int j = 0; j < cx; ++j)
+		int index = i;
+		for(int j = 0; j < cx; ++j)
 		{
-            int v = y->buffer[i] * x->buffer[j] + carry;
-            incrementDigit(&res, index++, v % 16);
-            carry = (v / 16);
-        }
-        incrementDigit(&res, index, carry);
-        carry = 0;
-    }
-    return res;
+			int v = y->buffer[i] * x->buffer[j] + carry;
+			incrementDigit(&res, index++, v % 16);
+			carry = (v / 16);
+		}
+		incrementDigit(&res, index, carry);
+		carry = 0;
+	}
+	return res;
 }
 
 // calculate x^y
 // x^y = x^(y/2) * x^(y/2) * (y%2)?x: 1
 BigInteger exponent(unsigned int x, unsigned int y)
 {
-    if (y == 0)
-        return create(1);
+	if (y == 0)
+		return create(1);
 
-    if (y == 1)
-        return create(x);
+	if (y == 1)
+		return create(x);
 
-    BigInteger half = exponent(x, y >> 1u);
+	BigInteger half = exponent(x, y >> 1u);
 
-    if (y & 1u) 
+	if (y & 1u) 
 	{
-        BigInteger tmp1 = create(x);
-        BigInteger tmp2 = multiply(&half, &half);
-        return multiply(&tmp1, &tmp2);
-    }
-    else
-        return multiply(&half, &half);
+		BigInteger tmp1 = create(x);
+		BigInteger tmp2 = multiply(&half, &half);
+		return multiply(&tmp1, &tmp2);
+	}
+	else
+		return multiply(&half, &half);
 }
 
 int main() 
 {
-    const char* description = "This program print's the solution of 3 ^ 5000"
-                              " in hexadecimal\n\n";
+	const char* description =	"This program print's the solution of 3 ^ 10000"
+								" in hexadecimal\n\n";
 
-    printf("%s", description);
-    BigInteger res = exponent(3, 5000);
-    print(&res);
+	printf("%s", description);
+	BigInteger res = exponent(3, 10000);
+	print(&res);
 }
