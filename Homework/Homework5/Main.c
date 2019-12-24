8b4e487c40b0ada054d845001a8f7e3870db04b5
Илья <i7ok@yandex.ru>
2019-12-24 11:32:35 +0300

    HW5Fixed.v5

--- Homework/Homework5/Main.c
+++ Homework/Homework5/Main.c
@@ -8,59 +8,61 @@ typedef uint32_t u32;
 
 int readNumberStdin(int maxDigits, u32* ret) 
 {
-    int digits = 0;
-    int c = 0;
-    (*ret) = 0;
+	int digits = 0;
+	int c = 0;
+	(*ret) = 0;
 
-    while ((c = getchar()) != '\n') 
+	while ((c = getchar()) != '\n') 
 	{
-        if (!(c >= '0' && c <= '9') || (++digits > maxDigits)) 
+		if (!(c >= '0' && c <= '9') || (++digits > maxDigits)) 
 		{
-            while (getchar() != '\n');
-            return 1;
-        }
-        (*ret) = (*ret) * 10 + (c - '0');
+			while (getchar() != '\n');
+			return 1;
+		}
+		(*ret) = (*ret) * 10 + (c - '0');
     }
 
-    return !digits;
+	return !digits;
 }
 
 int isPerfectSquare(u32 n) 
 {
-    u32 r = floor(sqrt(n));
-    return (r * r) == n;
+	u32 r = floor(sqrt(n));
+	return (r * r) == n;
 }
 
 int main()
 {
 
-    const char description[] = "This program finds the terms and period of continue"
-                               " fraction of sqrt(n)\n\n";
+	const char description[] = "This program finds the terms and period of continue"
+								" fraction of sqrt(n)\n\n";
 
-    printf("%s", description);
+	printf("%s", description);
 
-    u32 n;
-    do 
+	u32 n;
+	do 
 	{
-        printf("enter a non-perfect square number(max digits=8): ");
-    } while(readNumberStdin(8, &n) || (n == 0) || isPerfectSquare(n));
+		printf("enter a non-perfect square number(max digits=8): ");
+	}
+	while(readNumberStdin(8, &n) || (n == 0) || isPerfectSquare(n));
 
-    u32 r = floor(sqrt(n));
-    u32 a = r;
-    u32 p = 0;
-    u32 q = 1;
-    u32 i = 0;
+	u32 r = floor(sqrt(n));
+	u32 a = r;
+	u32 p = 0;
+	u32 q = 1;
+	u32 i = 0;
 
-    printf("\nsequence: [%d; ", r);
-    do 
+	printf("\nsequence: [%d; ", r);
+	do 
 	{    
-        p = a * q - p;
-        q = (n - p * p) / q;
-        a = (r + p) / q;
-        ++i;
-        printf("%d, ", a);
-    } while(q != 1);
-    printf("...]\nperiod: %d\n", i);
+		p = a * q - p;
+		q = (n - p * p) / q;
+		a = (r + p) / q;
+		++i;
+		printf("%d, ", a);
+	}
+	while(q != 1);
+	printf("...]\nperiod: %d\n", i);
 }
 
 
