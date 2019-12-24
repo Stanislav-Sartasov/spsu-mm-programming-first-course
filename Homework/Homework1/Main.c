eb7fbd8be961a5f6c0fade5d54a492de89ae419b
Илья <i7ok@yandex.ru>
2019-12-24 09:55:43 +0300

    Homework 1 fixed.v3

--- Homework/Homework1/Main.c
+++ Homework/Homework1/Main.c
@@ -2,24 +2,31 @@
 #include <stdint.h>
 #include <string.h>
 
-void printBinary32(uint32_t num) 
+void printBinary(uint64_t num)
 {
-    for(int i = 31; i >= 0; --i)
-        printf("%d", (num & (1u << i)) >= 1);
-    printf("\n");
+	for (int i = 31; i >= 0; --i)
+		printf("%d", (num & (1ull << i)) >= 1);
+	printf("\n");
 }
 
-void printBinary64(uint64_t num) 
-{
-    for(int i = 63; i >= 0; --i)
-        printf("%d", (num & (1ull << i)) >= 1);
-    printf("\n");
-}
+//void printBinary32(uint32_t num)
+//{
+//	for (int i = 31; i >= 0; --i)
+//		printf("%d", (num & (1u << i)) >= 1);
+//	printf("\n");
+//}
 
-uint32_t twoComplement(uint32_t num) 
-{
-    return (~num) + 1;
-}
+//void printBinary64(uint64_t num) 
+//{
+//    for(int i = 63; i >= 0; --i)
+//        printf("%d", (num & (1ull << i)) >= 1);
+//    printf("\n");
+//}
+
+//uint32_t twoComplement(uint32_t num) 
+//{
+//    return (~num) + 1;
+//}
 
 int main() 
 {
@@ -35,12 +42,12 @@ int main()
     double c = product;
 
     printf("The negative 32-bit integer: ");
-    printBinary32(twoComplement(a));
+    printBinary((~a) + 1);
 
     printf("The positive floating-point number: ");
-    printBinary32((*((uint32_t*)&b)));
+    printBinary((*((uint64_t*)&b)));
 
     printf("The negative floating-point number: ");
-    printBinary64((*((uint64_t*)&c)));
+    printBinary((*((uint64_t*)&c)));
 	
 }
