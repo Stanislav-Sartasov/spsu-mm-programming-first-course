16f5c90cb45f37aa6bf64d923c062007c4cc9589
Yunus Emre <lucifgy@yahoo.com>
2019-12-24 11:17:59 +0300

    Task8, fix

--- Homeworks/Task8/task8.c
+++ Homeworks/Task8/task8.c
@@ -1,199 +1,232 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define FILTER_AVERAGE		"average"
-#define FILTER_GAUSS3		"gauss3"
-#define FILTER_GAUSS5		"gauss5"
-#define FILTER_SOBELX		"sobelx"
-#define FILTER_SOBELY		"sobely"
-#define FILTER_GRAY			"grey"
+#define FILTER_AVERAGE       "average"
+#define FILTER_GAUSS3        "gauss3"
+#define FILTER_GAUSS5        "gauss5"
+#define FILTER_SOBELX        "sobelx"
+#define FILTER_SOBELY        "sobely"
+#define FILTER_GRAY          "gray"
 
-typedef unsigned int DWORD;
-typedef long LONG;
-typedef unsigned short WORD;
-typedef unsigned char BYTE;
 
-#pragma pack(1)
+#pragma pack(push, 1)
 
-typedef struct tagBITMAPINFOHEADER{
-	DWORD      biSize;
-	LONG       biWidth;
-	LONG       biHeight;
-	WORD       biPlanes;
-	WORD       biBitCount;
-	DWORD      biCompression;
-	DWORD      biSizeImage;
-	LONG       biXPelsPerMeter;
-	LONG       biYPelsPerMeter;
-	DWORD      biClrUsed;
-	DWORD      biClrImportant;
-} BITMAPINFOHEADER;
-
-typedef struct tagBITMAPFILEHEADER
+typedef struct
 {
-	WORD    bfType;
-	DWORD   bfSize;
-	WORD    bfReserved1;
-	WORD    bfReserved2;
-	DWORD   bfOffBits;
-} BITMAPFILEHEADER;
+	unsigned int biSize;
+	unsigned int biWidth;
+	unsigned int biHeight;
+	unsigned short biPlanes;
+	unsigned short biBitCount;
+	unsigned int biCompression;
+	unsigned int biSizeImage;
+	unsigned int biXPelsPerMeter;
+	unsigned int biYPelsPerMeter;
+	unsigned int biClrUsed;
+	unsigned int biClrImportant;
+} BitmapInfoHeader;
 
-typedef struct tagRGB
+typedef struct
 {
-	unsigned char rgbBlue;
-	unsigned char rgbGreen;
-	unsigned char rgbRed;
+	unsigned short bfType;
+	unsigned int bfSize;
+	unsigned short bfReserved1;
+	unsigned short bfReserved2;
+	unsigned int bfOffBits;
+} BitmapFileHeader;
+
+typedef struct
+{
+	unsigned char blue;
+	unsigned char green;
+	unsigned char red;
 } RGB;
 
-#pragma pack()
+#pragma pack(pop)
 
-int read_image_file(char* input_file, BITMAPFILEHEADER* file_header, BITMAPINFOHEADER* info_header, RGB** pImg, char** pPal, int* pPalSize, int* pStride)
+typedef struct
 {
-	FILE* fp;
-	int k;
+	BitmapFileHeader file_header;
+	BitmapInfoHeader info_header;
+	RGB *image_data;
+} BitmapImage;
 
-	if (NULL == (fp = fopen(input_file, "rb")))
+
+int read_image_file(char *input_file, BitmapImage *image)
+{
+	FILE *fp = fopen(input_file, "rb");
+	if (!fp)
 	{
 		printf("Can't open the input file.\n");
-		return 0;
+		return -1;
 	}
-	fread(file_header, sizeof(BITMAPFILEHEADER), 1, fp);
-	fread(info_header, sizeof(BITMAPINFOHEADER), 1, fp);
-	if (info_header->biBitCount != 24 && info_header->biBitCount != 32)
-	{
+
+	BitmapFileHeader *file_header = &image->file_header;
+	fread(file_header, sizeof(BitmapFileHeader), 1, fp);
+	if (file_header->bfType != 0x4D42) {
 		fclose(fp);
+		printf("Input file is not in bmp format.\n");
+		return -1;
+	}
+	BitmapInfoHeader *info_header = &image->info_header;
+	fread(info_header, sizeof(BitmapInfoHeader), 1, fp);
+
+	/*if (info_header->biBitCount != 24 && info_header->biBitCount != 32) {
+		fclose(fp);
+		printf("Input file is neither 32-bit or 24-bit.\n");
 		return 0;
+	}*/
+
+	fseek(fp, file_header->bfOffBits, SEEK_SET);
+
+	RGB *image_data = (RGB *)malloc(info_header->biWidth * info_header->biHeight * sizeof(RGB));
+
+	if (!image_data)
+	{
+		free(image);
+		fclose(fp);
+		printf("Couldnt allocate memory to store image");
+		return -1;
 	}
 
-	*pPalSize = file_header->bfOffBits - sizeof(BITMAPFILEHEADER) - sizeof(BITMAPINFOHEADER);
-	if (*pPalSize > 0)
+	int k = 0;
+	for (int i = 0; i < info_header->biHeight; i++)
 	{
-		*pPal = (char*)malloc(*pPalSize);
-		fread(*pPal, 1, *pPalSize, fp);
-	}
-
-	*pImg = (RGB*)malloc(info_header->biWidth * info_header->biHeight * sizeof(RGB));
-	*pStride = (4 - (info_header->biWidth * (info_header->biBitCount / 8)) % 4) & 3;
-
-	k = 0;
-	for (int i = 0 ; i < info_header->biHeight ; i++)
-	{
-		for (int j = 0 ; j < info_header->biWidth ; j++)
+		for (int j = 0; j < info_header->biWidth; j++)
 		{
-			fread(*pImg + k, sizeof(RGB), 1, fp);
-			k = k + 1;
-			if (info_header->biBitCount == 32)
-				fseek(fp, 1, SEEK_CUR);
+			fread(image_data + k++, sizeof(RGB), 1, fp);
 		}
-		fseek(fp, *pStride, SEEK_CUR);
 	}
-
+	image->image_data = image_data;
 	fclose(fp);
-	return 1;
+	return 0;
+
+
 }
 
-void apply_matrix_to_filter(RGB* img, int width, int height, int* matrix, int dim)
+int write_image_file(char *output_file, BitmapImage *image)
+{
+	FILE *fp = fopen(output_file, "wb");
+	if (!fp)
+	{
+		printf("Can't open the output file");
+		return -1;
+	}
+	fwrite(&image->file_header, sizeof(BitmapFileHeader), 1, fp);
+	fwrite(&image->info_header, sizeof(BitmapInfoHeader), 1, fp);
+	for (int i = 0; i < image->info_header.biWidth * image->info_header.biHeight; i++)
+		fwrite(&image->image_data[i], sizeof(RGB), 1, fp);
+	fclose(fp);
+	return 0;
+}
+
+void apply_matrix_to_filter(RGB *img, int width, int height, int *matrix, int dim)
 {
 	int i, j, k, l;
 	int r, g, b;
-	RGB* t;
+	RGB *t;
 	int gap, weight;
 
-	t = (RGB*)malloc(width * height * sizeof(RGB));
+	t = (RGB *)malloc(width * height * sizeof(RGB));
 	memcpy(t, img, width * height * sizeof(RGB));
 
 	gap = (dim - 1) / 2;
 
 	weight = 0;
-	for (i = -gap ; i <= gap ; i++)
+	for (i = -gap; i <= gap; i++)
 	{
-		for (j = -gap; j <= gap ; j++)
-		{
-			weight += matrix[(gap+i)*dim+gap+j];
+		for (j = -gap; j <= gap; j++) {
+			weight += matrix[(gap + i) * dim + gap + j];
 		}
 	}
 
-	for (i = gap ; i < height - gap ; i++)
+	for (i = gap; i < height - gap; i++)
 	{
-		for(j = gap ; j < width - gap ; j++)
+		for (j = gap; j < width - gap; j++)
 		{
-			r = 0; g = 0; b = 0;
-			for (k = -gap ; k <= gap ; k++)
+			r = 0;
+			g = 0;
+			b = 0;
+			for (k = -gap; k <= gap; k++)
 			{
-				for (l = -gap ; l <= gap ; l++)
-				{
-					r += t[(i+k)*width+j+l].rgbRed   * matrix[(gap+k)*dim+gap+l];
-					g += t[(i+k)*width+j+l].rgbGreen * matrix[(gap+k)*dim+gap+l];
-					b += t[(i+k)*width+j+l].rgbBlue  * matrix[(gap+k)*dim+gap+l];
+				for (l = -gap; l <= gap; l++) {
+					r += t[(i + k) * width + j + l].red * matrix[(gap + k) * dim + gap + l];
+					g += t[(i + k) * width + j + l].green * matrix[(gap + k) * dim + gap + l];
+					b += t[(i + k) * width + j + l].blue * matrix[(gap + k) * dim + gap + l];
 				}
 			}
 
-			img[i*width+j].rgbRed = r / weight;
-			img[i*width+j].rgbGreen = g / weight;
-			img[i*width+j].rgbBlue = b / weight;
+			img[i * width + j].red = r / weight;
+			img[i * width + j].green = g / weight;
+			img[i * width + j].blue = b / weight;
 		}
 	}
 
 	free(t);
 }
 
-void set_average_filter(RGB* img, int width, int height)
+void set_average_filter(RGB *img, int width, int height)
 {
 	int matrix[] = {
-		1, 1, 1, 
-		1, 1, 1, 
-	    1, 1, 1
+			1, 1, 1,
+			1, 1, 1,
+			1, 1, 1
 	};
 
 	apply_matrix_to_filter(img, width, height, matrix, 3);
 }
 
-void set_gauss_filter_3(RGB* img, int width, int height)
+void set_gauss_filter_3(RGB *img, int width, int height)
 {
-	int gauss3[] = {
-		1, 2, 1,
-		2, 4, 2,
-		1, 2, 1
+	int gauss3[] =
+	{
+			1, 2, 1,
+			2, 4, 2,
+			1, 2, 1
 	};
 
 	apply_matrix_to_filter(img, width, height, gauss3, 3);
 }
 
-void set_gauss_filter_5(RGB* img, int width, int height)
+void set_gauss_filter_5(RGB *img, int width, int height)
 {
-	int gauss5[] = {
-		1, 4,  6,  4,  1,
-		4, 16, 24, 16, 4,
-		6, 24, 36, 24, 6,
-		4, 16, 24, 16, 4,
-		1, 4,  6,  4,  1
+	int gauss5[] =
+	{
+			1, 4, 6, 4, 1,
+			4, 16, 24, 16, 4,
+			6, 24, 36, 24, 6,
+			4, 16, 24, 16, 4,
+			1, 4, 6, 4, 1
 	};
 
 	apply_matrix_to_filter(img, width, height, gauss5, 5);
 }
 
-void apply_sobel_filter(RGB* img, int width, int height, int* matrix)
+void apply_sobel_filter(RGB *img, int width, int height, int *matrix)
 {
 	int i, j, k, l;
 	int r, g, b;
-	RGB* t;
+	RGB *t;
 
-	t = (RGB*)malloc(width * height * sizeof(RGB));
+	t = (RGB *)malloc(width * height * sizeof(RGB));
 	memcpy(t, img, width * height * sizeof(RGB));
 
-	for (i = 1 ; i < height - 1 ; i++)
+	for (i = 1; i < height - 1; i++)
 	{
-		for(j = 1 ; j < width - 1 ; j++)
+		for (j = 1; j < width - 1; j++)
 		{
-			r = 0; g = 0; b = 0;
-			for (k = -1 ; k <= 1 ; k++)
+			r = 0;
+			g = 0;
+			b = 0;
+			for (k = -1; k <= 1; k++)
 			{
-				for (l = -1 ; l <= 1 ; l++)
+				for (l = -1; l <= 1; l++)
 				{
-					r += t[(i+k)*width+j+l].rgbRed   * matrix[(1+k)*3+1+l];
-					g += t[(i+k)*width+j+l].rgbGreen * matrix[(1+k)*3+1+l];
-					b += t[(i+k)*width+j+l].rgbBlue  * matrix[(1+k)*3+1+l];
+					r += t[(i + k) * width + j + l].red * matrix[(1 + k) * 3 + 1 + l];
+					g += t[(i + k) * width + j + l].green * matrix[(1 + k) * 3 + 1 + l];
+					b += t[(i + k) * width + j + l].blue * matrix[(1 + k) * 3 + 1 + l];
 				}
 			}
 
@@ -204,126 +237,99 @@ void apply_sobel_filter(RGB* img, int width, int height, int* matrix)
 			if (b < 0) b = 0;
 			if (b > 255) b = 255;
 
-			img[i*width+j].rgbRed = r;
-			img[i*width+j].rgbGreen = g;
-			img[i*width+j].rgbBlue = b;
+			img[i * width + j].red = r;
+			img[i * width + j].green = g;
+			img[i * width + j].blue = b;
 		}
 	}
 
 	free(t);
 }
 
-void set_sobel_filter_x(RGB* img, int width, int height)
+void set_sobel_filter_x(RGB *img, int width, int height)
 {
 	int sobelx[] = {
-		1,  2,  1,
-		0,  0,  0,
-		-1, -2, -1
+			1, 2, 1,
+			0, 0, 0,
+			-1, -2, -1
 	};
 
 	apply_sobel_filter(img, width, height, sobelx);
 }
 
-void set_sobel_filter_y(RGB* img, int width, int height)
+void set_sobel_filter_y(RGB *img, int width, int height)
 {
-	int sobely[] = {
-		-1, 0, 1,
-		-2, 0, 2,
-		-1, 0, 1
+	int sobely[] =
+	{
+			-1, 0, 1,
+			-2, 0, 2,
+			-1, 0, 1
 	};
 
 	apply_sobel_filter(img, width, height, sobely);
 }
 
-void set_grey_filter(RGB* img, int width, int height)
+void set_grey_filter(RGB *img, int width, int height)
 {
 	int i, j;
 	int val;
 
-	for (i = 0 ; i < height ; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0 ; j < width ; j++)
-		{
-			val = (img[i*width+j].rgbRed + img[i*width+j].rgbGreen + img[i*width+j].rgbBlue) / 3;
-			img[i*width+j].rgbRed = val;
-			img[i*width+j].rgbGreen = val;
-			img[i*width+j].rgbBlue = val;
+		for (j = 0; j < width; j++) {
+			val = (img[i * width + j].red + img[i * width + j].green + img[i * width + j].blue) / 3;
+			img[i * width + j].red = val;
+			img[i * width + j].green = val;
+			img[i * width + j].blue = val;
 		}
 	}
 }
 
-void write_image_file(char* output_file, BITMAPFILEHEADER* file_header, BITMAPINFOHEADER* info_header, RGB* img, char* pal, int palSize, int pStride)
+int main(int argc, char *argv[])
 {
-	FILE* fp;
-
-	if ((fp = (fopen(output_file, "wb"))) == NULL)
-	{
-		printf("Can't open the output file");
-		return;
-	}
-	fwrite(file_header, sizeof(BITMAPFILEHEADER), 1, fp);
-	fwrite(info_header, sizeof(BITMAPINFOHEADER), 1, fp);
-
-	if (palSize)
-	{
-		fwrite(pal, 1, palSize, fp);
-	}
-
-	fwrite(img, sizeof(RGB), info_header->biHeight * info_header->biWidth, fp);
-
-	fclose(fp);
-}
-
-int main(int argc, char* argv[])
-{
-	BITMAPFILEHEADER file_header;
-	BITMAPINFOHEADER info_header;
-	RGB* img;
-	char* pal = 0;
-	int pal_size, stride;
-
 	if (argc != 4)
 	{
 		printf("<exe_name> FILTER_NAME INPUT_BMP OUTPUT_BMP\n");
 		printf("for example\n");
-		printf("task8.exe %s imput.bmp output.bmp\n", FILTER_AVERAGE);
-		printf("task8.exe %s imput.bmp output.bmp\n", FILTER_GAUSS3);
-		printf("task8.exe %s imput.bmp output.bmp\n", FILTER_GAUSS5);
-		printf("task8.exe %s imput.bmp output.bmp\n", FILTER_SOBELX);
-		printf("task8.exe %s imput.bmp output.bmp\n", FILTER_SOBELY);
-		printf("task8.exe %s imput.bmp output.bmp\n", FILTER_GRAY);
+		printf("a.out %s imput.bmp output.bmp\n", FILTER_AVERAGE);
+		printf("a.out %s imput.bmp output.bmp\n", FILTER_GAUSS3);
+		printf("a.out %s imput.bmp output.bmp\n", FILTER_GAUSS5);
+		printf("a.out %s imput.bmp output.bmp\n", FILTER_SOBELX);
+		printf("a.out %s imput.bmp output.bmp\n", FILTER_SOBELY);
+		printf("a.out %s imput.bmp output.bmp\n", FILTER_GRAY);
 		return -1;
 	}
 
-	if (0 == read_image_file(argv[2], &file_header, &info_header, &img, &pal, &pal_size, &stride))
+	BitmapImage image;
+	if (read_image_file(argv[2], &image) == -1)
 	{
-		printf("The bmp file is invalid.\n");
+		printf("Invalid input file.");
 		return -1;
 	}
 
 	if (strcmp(argv[1], FILTER_AVERAGE) == 0)
 	{
-		set_average_filter(img, info_header.biWidth, info_header.biHeight);
+		set_average_filter(image.image_data, image.info_header.biWidth, image.info_header.biHeight);
 	}
 	else if (strcmp(argv[1], FILTER_GAUSS3) == 0)
 	{
-		set_gauss_filter_3(img, info_header.biWidth, info_header.biHeight);
+		set_gauss_filter_3(image.image_data, image.info_header.biWidth, image.info_header.biHeight);
 	}
 	else if (strcmp(argv[1], FILTER_GAUSS5) == 0)
 	{
-		set_gauss_filter_5(img, info_header.biWidth, info_header.biHeight);
+		set_gauss_filter_5(image.image_data, image.info_header.biWidth, image.info_header.biHeight);
 	}
 	else if (strcmp(argv[1], FILTER_SOBELX) == 0)
 	{
-		set_sobel_filter_x(img, info_header.biWidth, info_header.biHeight);
+		set_sobel_filter_x(image.image_data, image.info_header.biWidth, image.info_header.biHeight);
 	}
 	else if (strcmp(argv[1], FILTER_SOBELY) == 0)
 	{
-		set_sobel_filter_y(img, info_header.biWidth, info_header.biHeight);
+		set_sobel_filter_y(image.image_data, image.info_header.biWidth, image.info_header.biHeight);
 	}
 	else if (strcmp(argv[1], FILTER_GRAY) == 0)
 	{
-		set_grey_filter(img, info_header.biWidth, info_header.biHeight);
+		set_grey_filter(image.image_data, image.info_header.biWidth, image.info_header.biHeight);
 
 	}
 	else
@@ -331,13 +337,12 @@ int main(int argc, char* argv[])
 		printf("The filter name is invalid.\n");
 		return -1;
 	}
-
-	write_image_file(argv[3], &file_header, &info_header, img, pal, pal_size, stride);
-
-	if (img) free(img);
-	if (pal) free(pal);
-
+	if (write_image_file(argv[3], &image) == -1)
+	{
+		printf("Invalid output file.");
+		free(image.image_data);
+		return -1;
+	}
+	free(image.image_data);
 	return 0;
 }
-
-
