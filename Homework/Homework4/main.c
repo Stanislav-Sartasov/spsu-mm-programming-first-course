8e10bd8d9d32e1785b3a25acdcb6e4a7902d9337
Илья <i7ok@yandex.ru>
2019-12-24 11:29:49 +0300

    HW4Fixed.v4

--- Homework/Homework4/main.c
+++ Homework/Homework4/main.c
@@ -5,25 +5,25 @@ uint32_t pows[32];
 
 void calculatePow2() 
 {
-    pows[0] = 1;
-    for(int i = 1; i < 32; ++i) 
+	pows[0] = 1;
+	for(int i = 1; i < 32; ++i) 
 	{
-        pows[i] = pows[i - 1] * 2;
-    }
+		pows[i] = pows[i - 1] * 2;
+	}
 }
 
 int main() 
 {
-    const char* description = "This program print's all the Mersenne primes in"
-                              " interval [1, 2^31 - 1]\n";
+	const char* description = "This program print's all the Mersenne primes in"
+								" interval [1, 2^31 - 1]\n";
 
-    printf("%s", description);
+	printf("%s", description);
 
-    calculatePow2();
+	calculatePow2();
 
 //    int mersenneExp[8] = {2, 3, 5, 7, 13, 17, 19, 31};
 
-    for (int i = 0; i < 32; ++i) 
+	for (int i = 0; i < 32; ++i) 
 	{
 		printf("%d\n", pows[i] - 1);
     }
