6f1ebb4448ad9ea6fbd1831f2681bcbcba6ab537
Yunus Emre <lucifgy@yahoo.com>
2019-12-24 18:29:02 +0300

    Task7 fixed

--- Homeworks/Task7/hashmap.h
+++ Homeworks/Task7/hashmap.h
@@ -1,16 +1,18 @@
 #ifndef PROJECT_HASHMAP_H
 #define PROJECT_HASHMAP_H
 
-typedef struct Entry {
-    int key;
-    int value;
-    struct Entry *next;
+typedef struct Entry
+{
+	int key;
+	int value;
+	struct Entry *next;
 } Entry;
 
-typedef struct HashTable {
-    int size;
-    int defaultValue;
-    struct Entry **baskets;
+typedef struct HashTable 
+{
+	int size;
+	int defaultValue;
+	struct Entry **baskets;
 } HashTable;
 
 int hashCode(int key, int n);
@@ -21,12 +23,9 @@ int lookup(HashTable *hashTable, int key);
 
 void delete(HashTable *hashTable, int key);
 
-void rebalance(HashTable *hashTable);
-
 HashTable *createHashTable(int size, int defaultValue);
 
 void freeTable(HashTable *hashTable);
 
-void print(HashTable *hashTable);
 
 #endif //PROJECT_HASHMAP_H
--- Homeworks/Task7/task7.c
+++ Homeworks/Task7/task7.c
@@ -5,37 +5,61 @@
 #include <time.h>
 
 
-int main() {
+void print(HashTable *hashTable)
+{
+	for (int i = 0; i < hashTable->size; ++i) 
+	{
+		printf("Basket%d: ", i);
+		Entry *entry = hashTable->baskets[i];
+		while (entry)
+		{
+			printf("(%d, %d) ", entry->key, entry->value);
+			entry = entry->next;
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+}
+
+
+int main()
+{
 	char str[1000];
 	int initialSize;
 	int dataSize;
 	int max = 20;
 	srand(time(0));
-	while (1) {
+	while (1)
+	{
 		printf("Enter hash table initial size: ");
 		scanf("%s", str);
 
 		initialSize = atoi(str);
 
-		if (initialSize > 0) {
+		if (initialSize > 0)
+		{
 			break;
 		}
 		printf("You entered incorrect value\n");
 	}
-	while (1) {
+	while (1) 
+	{
 		printf("Enter random data size: ");
 		scanf("%s", str);
 
 		dataSize = atoi(str);
 
-		if (dataSize > 0) {
+		if (dataSize > 0)
+		{
 			break;
 		}
 		printf("You entered incorrect value\n");
 	}
 	HashTable *table = createHashTable(initialSize, 0);
 
-	for (int i = 0; i < dataSize; i++) {
+	for (int i = 0; i < dataSize; i++)
+	{
 		int key = rand() % max;
 		int value = rand();
 		printf("Inserting key=%d value=%d in table\n", key, value);
@@ -43,13 +67,17 @@ int main() {
 		print(table);
 	}
 
-	for (int i = 0; i < max; ++i) {
-		if (lookup(table, i) != 0) {
+	for (int i = 0; i < max; ++i)
+	{
+		if (lookup(table, i) != 0) 
+		{
 			printf("Deleting key=%d from table\n", i);
 			delete(table, i);
 			print(table);
 		}
 	}
+	freeTable(table);
+
 	return 0;
 }
 
