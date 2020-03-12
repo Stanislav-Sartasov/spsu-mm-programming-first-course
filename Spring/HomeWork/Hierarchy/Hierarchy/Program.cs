e921104ca2073f073e2309ef912652b3febb68c6
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2020-03-12 22:28:25 +0300

    Add files via upload

---
+++ Spring/HomeWork/Hierarchy/Hierarchy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Panter panter = new Panter();
+            panter.Print();
+            panter.Print();
+        }
+    }
+}
---
+++ Spring/HomeWork/Hierarchy/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Hierarchy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void T34()
+        {
+            T34 t34Test = new T34();
+            Assert.AreEqual("Black", t34Test.Color);
+            Assert.AreEqual("Ussr", t34Test.CountryOfManufacture);
+            Assert.AreEqual(56, t34Test.MaxSpeed);
+            Assert.AreEqual(400, t34Test.Armor);
+            Assert.AreEqual(4, t34Test.CabinCrew);
+
+        }
+
+        public void T49()
+        {
+            T49 t49Test = new T49();
+            Assert.AreEqual("Brown", t49Test.Color);
+            Assert.AreEqual("the USA", t49Test.CountryOfManufacture);
+            Assert.AreEqual(65, t49Test.MaxSpeed);
+            Assert.AreEqual(1300, t49Test.Armor);
+            Assert.AreEqual(4, t49Test.CabinCrew);
+
+        }
+        public void Panter()
+        {
+            Panter PanterTest = new Panter();
+            Assert.AreEqual("Gre", PanterTest.Color);
+            Assert.AreEqual("German", PanterTest.CountryOfManufacture);
+            Assert.AreEqual(55, PanterTest.MaxSpeed);
+            Assert.AreEqual(1200, PanterTest.Armor);
+            Assert.AreEqual(5, PanterTest.CabinCrew);
+
+        }
+    }
+}
