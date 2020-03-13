06d5aa30fd15954f82396e31d447deea3feaba53
makar-pelogeiko <m.pelogeiko@mail.ru>
2020-03-14 00:08:19 +0300

    задача "простая иерархия"

---
+++ term_2/c#/tank/tank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using manyTanks;
+using abstractTank;
+
+namespace tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First tank:");
+            anyTank tank = new t34();
+            tank.getInfo();
+
+            Console.WriteLine("Second tank:");
+            
+            anyTank secTank = new m4();
+            secTank.getInfo();
+
+        }
+    }
+}
