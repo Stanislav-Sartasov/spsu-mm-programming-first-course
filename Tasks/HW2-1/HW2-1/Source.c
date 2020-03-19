551decefada2b3705afca6cb2380e9ee35711033
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2020-03-19 12:48:58 +0300

    Add files via upload

--- Tasks/HW2-1/HW2-1/Source.c
+++ Tasks/HW2-1/HW2-1/Source.c
@@ -10,94 +10,96 @@
 
 int cmp(const void* p1, const void* p2)
 {
-	const char* s1, * s2;
+    char* s1, * s2;
 
-	s1 = *(char**)p1;
-	s2 = *(char**)p2;
-	return strcmp(s1, s2);
+    s1 = *(char**)p1;
+    s2 = *(char**)p2;
+    while (*s1 == ' ') s1++;
+    while (*s2 == ' ') s2++;
+    return strcmp(s1, s2);
 }
 
 int main(int argc, char* argv[])
 {
-	int fin, fout, i, stringNumber = 1;
-	struct stat statbuf;
-	char* map;
-	char** strings;
+    int fin, fout, i, stringNumber = 1;
+    struct stat statbuf;
+    char* map;
+    char** strings;
 
-	if (argc != 3)
-	{
-		printf("insufficiently passed parameters");
-		return 1;
-	}
+    if (argc != 3)
+    {
+        printf("insufficiently passed parameters");
+        return 1;
+    }
 
-	if ((fin = _open(argv[1], O_RDWR)) == -1)
-	{
-		printf("unable to open file");
-		return 1;
-	}
+    if ((fin = _open(argv[1], O_RDWR)) == -1)
+    {
+        printf("unable to open file");
+        return 1;
+    }
 
-	if ((fout = _open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IWRITE)) == -1)
-	{
-		printf("unable create for recording");
-		return 1;
-	}
+    if ((fout = _open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IWRITE)) == -1)
+    {
+        printf("unable create for recording");
+        return 1;
+    }
 
-	fstat(fin, &statbuf);
+    fstat(fin, &statbuf);
 
-	map = mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fin, 0);
+    map = mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fin, 0);
 
-	if (map == MAP_FAILED)
-	{
-		printf("map error");
-		return 1;
-	}
+    if (map == MAP_FAILED)
+    {
+        printf("map error");
+        return 1;
+    }
 
-	for (i = 0; i < statbuf.st_size; i++)
-	{
-		if (map[i] == '\n')
-			stringNumber++;
-	}
+    for (i = 0; i < statbuf.st_size; i++)
+    {
+        if (map[i] == '\n')
+            stringNumber++;
+    }
 
-	strings = (char**)malloc(sizeof(char*) * stringNumber);
+    strings = (char**)malloc(sizeof(char*) * stringNumber);
 
-	strings[0] = &map[0];
+    strings[0] = &map[0];
 
-	int j = 0;
+    int j = 0;
 
-	for (i = 0; i < statbuf.st_size; i++)
-	{
-		if (map[i] == '\n')
-		{
-			j++;
-			strings[j] = &map[i + 1];
-		}
-	}
+    for (i = 0; i < statbuf.st_size; i++)
+    {
+        if (map[i] == '\n')
+        {
+            j++;
+            strings[j] = &map[i + 1];
+        }
+    }
 
-	qsort(strings, stringNumber, sizeof(char*), cmp);
+    qsort(strings, stringNumber, sizeof(char*), cmp);
 
-	char slashN = '\n';
+    char slashN = '\n';
 
-	int len;
+    int len;
 
-	for (i = 0; i < stringNumber; i++)
-	{
-		len = 0;
-		while (*(strings[i] + len) != '\r' && *(strings[i] + len) != '\n' && *(strings[i] + len) != '\0')
-		{
-			if (&(*(strings[i] + len)) == &map[statbuf.st_size - 1])
-			{
-				len++;
-				break;
-			}
-			len++;
-		}
-		write(fout, strings[i], len);
-		write(fout, &slashN, 1);
-	}
-	free(strings);
-	munmap(map, statbuf.st_size);
-	close(fin);
-	close(fout);
-	system("pause");
-	return 0;
+    for (i = 0; i < stringNumber; i++)
+    {
+        len = 0;
+        while (*(strings[i] + len) != '\r' && *(strings[i] + len) != '\n' && *(strings[i] + len) != '\0')
+        {
+            if (&(*(strings[i] + len)) == &map[statbuf.st_size - 1])
+            {
+                len++;
+                break;
+            }
+            len++;
+        }
+        write(fout, strings[i], len);
+        write(fout, &slashN, 1);
+    }
+    free(strings);
+    munmap(map, statbuf.st_size);
+    close(fin);
+    close(fout);
+    system("pause");
+    return 0;
 }
