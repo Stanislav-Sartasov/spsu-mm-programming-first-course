1fcd893a1598394906ea5e2bd50f187599a63e5c
kirill-ivanov-a <ivanov.krirll.a@mail.com>
2020-03-19 22:21:48 +0300

    task "Blackjack" added

---
+++ Spring/Homework/Blackjack/Blackjack/Deck.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blackjack
+{
+    public class Deck
+    {
+        public List<Card> Cards { get; private set; }
+
+        public int NumOfCards { get; private set; }
+        public Deck(int number)
+        {
+            Cards = new List<Card>();
+            NumOfCards = number;
+            for (int i = 0; i < number; i++)
+            {
+                foreach (var cardSuit in Enum.GetValues(typeof(CardSuit)).Cast<CardSuit>())
+                {
+                    foreach (var cardValue in Enum.GetValues(typeof(CardValue)).Cast<CardValue>())
+                        Cards.Add(new Card(cardSuit, cardValue));
+                }
+            }
+            Shuffle();
+        }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+            NumOfCards = 8;
+            for (int i = 0; i < NumOfCards; i++) //default value of decks is 8
+            {
+                foreach (var cardSuit in Enum.GetValues(typeof(CardSuit)).Cast<CardSuit>())
+                {
+                    foreach (var cardValue in Enum.GetValues(typeof(CardValue)).Cast<CardValue>())
+                        Cards.Add(new Card(cardSuit, cardValue));
+                }
+            }
+            Shuffle();
+        }
+
+        public void Reset()
+        {
+            Cards.Clear();
+            Cards = new List<Card>();
+
+            for (int i = 0; i < NumOfCards; i++) //default value of decks is 8
+            {
+                foreach (var cardSuit in Enum.GetValues(typeof(CardSuit)).Cast<CardSuit>())
+                {
+                    foreach (var cardValue in Enum.GetValues(typeof(CardValue)).Cast<CardValue>())
+                        Cards.Add(new Card(cardSuit, cardValue));
+                }
+            }
+            Shuffle();
+        }
+
+        private void Shuffle()
+        {
+            int randomIndex;
+            var rand = new Random();
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                randomIndex = rand.Next(0, Cards.Count - 1);
+
+                Card oldCard = Cards[i];
+                Cards[i] = Cards[randomIndex];
+                Cards[randomIndex] = oldCard;
+            }
+        }
+
+        public Card GetCard()
+        {
+            int lastIndex = Cards.Count - 1;
+            Card popCard = Cards[lastIndex];
+            Cards.Remove(Cards[lastIndex]);
+            return popCard;
+        }
+    }
+}
---
+++ Spring/Homework/Blackjack/Blackjack/IBotPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack
+{
+    interface IBotPlayer
+    {
+        Command BotCommand(List<Command> availableCommands, Card openCroupierCard, int numOfHand);
+        int BotBet(int minBet, int chips);
+    }
+}
