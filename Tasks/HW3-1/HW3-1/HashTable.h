9d3ef42cbd2cbd547834c8f27eb137601f8c9fd0
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2020-03-20 14:29:17 +0300

    Add files via upload

---
+++ Tasks/HW3-1/HW3-1/HashTable.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+typedef struct node
+{
+	int value;
+	int key;
+	struct node* next;
+} node;
+
+typedef struct hashTable
+{
+	int numOfLists;
+	int maxLenOfList;
+	int* lenOfList;
+	node** arrayOfLists;
+} hashTable;
+
+void initHashTable(hashTable** table, int num);
+void tableResize(hashTable** table);
+int addPair(hashTable** table, int key, int value);
+void deletePair(hashTable* table, int key);
+int findValue(hashTable* table, int key);
+void printAllPairs(hashTable* table);
+void deleteHashTable(hashTable** table);
