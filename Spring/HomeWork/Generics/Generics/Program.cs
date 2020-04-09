2fad5b849c38966ee642d82a4a4d301f73bf4fbb
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2020-04-09 19:04:49 +0300

    Add files via upload

---
+++ Spring/HomeWork/Generics/Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> test = new Tree<int>();
+            test.add(5, 10);
+            test.add(5, 4);
+            test.add(5, 15);
+            test.add(5, 3);
+            test.add(5, 18);
+            test.add(5, 12);
+            test.add(5, 7);
+            
+
+            test.delete(15);
+            test.delete(4);
+
+            Console.WriteLine(test.root.left.key);
+           
+
+            test.find(7);
+
+           
+
+        }
+    }
+}
