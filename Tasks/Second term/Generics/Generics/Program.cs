9082c2a4f81c0edcdb91df95779d78f5ac8c058b
Krekep <pashaalimov@gmail.com>
2020-04-11 20:29:42 +0300

    Task 4 added

---
+++ Tasks/Second term/Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DynamicArray;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrange
+            DynamicArray<int> dynamicArray = new DynamicArray<int>();
+
+            //act
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Delete(1);
+            int actual = dynamicArray.GetAtIndex(1);
+            int expected = 2;
+            Console.WriteLine(actual);
+            DynamicArray<int>.PrintArray(dynamicArray);
+        }
+    }
+}
---
+++ Tasks/Second term/Generics/GenericsUnitTest/UnitTest.cs
@@ -0,0 +1,73 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicArray;
+using System;
+
+namespace GenericsUnitTest
+{
+    [TestClass]
+    public class UnitTest
+    { 
+        [TestMethod]
+        public void TestDynamicArrayCreateForObject()
+        {
+            try
+            {
+                DynamicArray<object> dynamicArray = new DynamicArray<object>();
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void TestDynamicArrayAddAndGetAtIdex()
+        {
+            //arrange
+            DynamicArray<int> dynamicArray = new DynamicArray<int>();
+
+            //act
+            dynamicArray.Add(2);
+            int actual = dynamicArray.GetAtIndex(0);
+            int expected = 2;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDynamicArrayDeleteByIndex()
+        {
+            //arrange
+            DynamicArray<int> dynamicArray = new DynamicArray<int>();
+
+            //act
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Delete(1);
+            int actual = dynamicArray.GetAtIndex(1);
+            int expected = 3;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestDynamicArrayFind()
+        {
+            //arrange
+            DynamicArray<int> dynamicArray = new DynamicArray<int>();
+
+            //act
+            dynamicArray.Add(1);
+            dynamicArray.Add(2);
+            dynamicArray.Add(3);
+            dynamicArray.Add(4);
+            int actual = dynamicArray.Find(3);
+            int expected = 2;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
