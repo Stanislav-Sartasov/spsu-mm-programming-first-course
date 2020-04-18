2647696cd32e645f521ee51a6655bda5612fd0e1
Ilias Mzoughi <i7ok@yandex.ru>
2020-04-18 13:13:14 +0300

    HW6Fixed.v5

--- Homework/Homework6/main.c
+++ Homework/Homework6/main.c
@@ -1,159 +1,165 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include "mman.h"
+﻿#include <stdio.h>
 #include <fcntl.h>
-#include <string.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+#include "mman.h"
 
+#define N 50
 
-/*
-	Compare two char objects.
-	@param p1:		The first char.
-	@param p2:		The second char.
-	@return int:	Returns an integral value indicating the relationship between the strings:
-			<0:		The first character that does not match has a lower value in p1 than in p2,
-			0:		The contents of both strings are equal,
-			>0:		The first character that does not match has a greater value in p1 than in p2.
-*/
-int charCmp(const void* p1, const void* p2)
+typedef struct
 {
-	const char* s1;
-	const char* s2;
-
-	s1 = *(char**)p1;
-	s2 = *(char**)p2;
-	return(strcmp(s1, s2));
+	char* Line;				// указатель на строку
+	unsigned int Length;	// длина строки
+} String;
+// вывод на печать строки
+void StringPrint(String* string)
+{
+	for (size_t index = 0; index < string->Length; ++index)
+		printf("%c", string->Line[index]);
+}
+// сравнение двух по содержимому
+int StringCompare(String* first, String* second)
+{
+	return strncmp(first->Line, second->Line, 
+		first->Length < second->Length ? first->Length : second->Length);
 }
 
-void printUsage() 
-{
-	const char description[] = "Usage: progname <input> <output>\n";
-	printf("%s", description);
-}
-
-// The main program starts here.
 int main(int argc, const char** argv)
 {
-
-	if (argc != 3) 
+	char inFileName[N] = { 0 }, outFileName[N] = { 0 };
+	
+	// считывание названий файлов из параметров для exe файла или
+	// при их несоответствии из консоли
+	if (argc == 3)
 	{
-		printUsage();
-		return 0;
-    }
+		for (uint32_t index = 0; argv[1][index] != 0; ++index)
+			inFileName[index] = argv[1][index];
+		for (uint32_t index = 0; argv[2][index] != 0; ++index)
+			outFileName[index] = argv[2][index];
+	}
+	else {
+		printf("Enter file name to read: ");
+		scanf_s("%s", inFileName, N - 1);
+		printf("Enter file name to write: ");
+		scanf_s("%s", outFileName, N - 1);
+	}
+	printf("You have entered: %s, %s.\n", inFileName, outFileName);
 
-	const char* input = argv[1];
-	const char* output = argv[2];
-    
-	int flin, flout, flinSize;
-	char* flinMapped;
-	struct stat flinInfo;
-	int i, k, len, j, maxLine, m, u;
-
-	flin = _open(input, O_RDWR, 0);
-	flout = _open(output, O_WRONLY | O_CREAT | O_TRUNC, S_IWRITE);
-
-	if (flin == -1) 
+	// открытие файлов для чтения (дескриптор inFile) и для записи (дескриптор outFile)
+	FILE* inFile = NULL, * outFile = NULL;
+	if (fopen_s(&inFile, inFileName, "r+") != 0)
 	{
-		printf("Unable to open file.\n");
-		exit(0);
+		printf("%s could not be opened for reading.\n", inFileName);
+		return -1;
+	}
+	if (fopen_s(&outFile, outFileName, "w+") != 0)
+	{
+		printf("%s could not be opened for writing.\n", outFileName);
+		return -1;
 	}
 
-	fstat(flin, &flinInfo);
-	flinSize = flinInfo.st_size;
-	flinMapped = mmap(0, flinSize, PROT_READ | PROT_WRITE, MAP_PRIVATE, flin, 0);
-	len = strlen(flinMapped);
-	u = 0;
-	m = 0;
-	maxLine = 0;
-
-	for (i = 0; i < len; i++) 
+	// если в конце файла нет символа конца строки, то добавляем его
+	// иначе не будет перевода на новую строку после сортировки
+	if (fseek(inFile, -1, SEEK_END) != 0)
 	{
-		if (flinMapped[i] != '\n') 
-		{
-			m++;
-			if (m > maxLine) 
-			{
-				maxLine = m;
-			}
-		}
-		else
-		{
-			m++;
-			u++;
-			if (m > maxLine)
-			{
-				maxLine = m;
-			}
-			m = 0;
-		}
+		printf("Could not size the input file.\n");
+		fclose(inFile);
+		fclose(outFile);
+		return -1;
+	}
+	char symbol;
+	fread(&symbol, 1, 1, inFile);
+	if (symbol != '\n')
+	{
+		fwrite("\n", 1, 1, inFile);
+		fclose(inFile);
+		fopen_s(&inFile, inFileName, "r");
 	}
 
-	char** linePtr = (char**)malloc(u * sizeof(char*));
-	char* str = (char*)malloc(maxLine * sizeof(char));
-
-	if (flinMapped == MAP_FAILED) 
+	// считываем информацию о размере файла для чтения
+	struct stat fileStat;
+	if (fstat(_fileno(inFile), &fileStat) != 0)
 	{
-		printf("Unable to map file.\n");
-		exit(1);
+		printf("Could not get the file size.\n");
+		fclose(inFile);
+		fclose(outFile);
+		return -1;
+	}
+	else
+		printf("The size of the file %s is %ld.\n", inFileName,	 fileStat.st_size);
+	// устанавливаем соответствующий размер файла для записи
+	if (fseek(outFile, fileStat.st_size, SEEK_SET) != 0)
+	{
+		printf("Could not size the output file.\n");
+		fclose(inFile);
+		fclose(outFile);
+		return -1;
+	}
+	fwrite(" ", 1, 1, outFile);
+
+	// отображение файлов в память
+	char* source, * destin;
+	source = (char*)mmap(0, fileStat.st_size, PROT_READ, MAP_SHARED, _fileno(inFile), 0);
+	if (source == MAP_FAILED)
+	{
+		printf("Could not map file %s.\n", inFileName);
+		fclose(inFile);
+		fclose(outFile);
+		return -1;
+	}
+	destin = (char*)mmap(0, fileStat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, _fileno(outFile), 0);
+	if (destin == MAP_FAILED)
+	{
+		printf("Could not map file %s.\n", outFile);
+		fclose(inFile);
+		fclose(outFile);
+		return -1;
 	}
 
-	k = 0;
-	for (i = 0; i < u; ) 
-	{
-		j = 0;
-		while (k < len) 
-		{
-			if (flinMapped[k] == '\n') 
-			{
-				str[j] = '\n';
-				j++;
-				k++;
-				break;
-			}
-            str[j] = flinMapped[k];
-            j++;
-            k++;
-		}
+	// проводим манипуляции по сортировке строк
 
-//		linePtr[i++] = _strdup(str);
-		linePtr[i++] = str;
-		if (k == len)
+	size_t count = 1;
+	// рассчитываем количество строк
+	for (size_t index = 0; index < fileStat.st_size; ++index)
+	{
+		if (source[index] == '\n')
+			count++;
+	}
+	// массив строк представления
+	String* lines = (String*)malloc((count + 1) * sizeof(String));
+	lines[0].Line = &(source[0]);
+	count = 0;
+	size_t length = 0;
+
+	// запоминаем указатели на начала строк файла и их длины
+	for (size_t index = 0; source[index] != 0; ++index)
+	{
+		length++;
+		if (source[index] == '\n')
 		{
-			break;
+			lines[count].Length = length;
+			length = 0;
+			lines[++count].Line = &(source[index + 1]);
 		}
 	}
+	lines[count].Length = length + 1;
 
-	qsort(linePtr, u, sizeof(char*), charCmp);
-	char* floutMapped = (char*)malloc(len * sizeof(char*));
-	k = 0;
-
-	for (i = 0; i < u; i++) 
+	qsort(lines, count + 1, sizeof(String), StringCompare);
+	
+	length = 0;
+	for (size_t index = 0; index < count + 1; ++index) 
 	{
-		char* tmp = linePtr[i];
-		while (*tmp != '\n')
-		{
-			floutMapped[k] = *tmp;
-			k++;
-			tmp++;
-		}
-
-		if (*tmp == '\n')
-		{
-			floutMapped[k] = '\n';
-			k++;
-		}
-
-		free(linePtr[i]);
+		for (size_t number = 0; number < lines[index].Length; ++number)
+			destin[length + number] = lines[index].Line[number];
+		length += lines[index].Length;
 	}
 
-	_write(flout, floutMapped, len);
-	munmap(flinMapped, len);
+	//for (unsigned int index = 0; source)
 
-	free(floutMapped);
-	free(linePtr);
-	free(str);
+	free(lines);
+	fclose(inFile);
+	fclose(outFile);
 
-	_close(flin);
-	_close(flout);
-	return(0);
+	return 0;
 }
