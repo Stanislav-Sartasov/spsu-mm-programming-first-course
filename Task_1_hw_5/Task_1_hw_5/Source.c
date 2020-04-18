2e6c2ffd52749b5780f71b238419ccf8d99dd09f
Andrey1404 <andreyschemyackin@yandex.ru>
2020-04-18 13:05:02 +0300

    Добавлена 10 задача

--- Task_1_hw_5/Task_1_hw_5/Source.c
+++ Task_1_hw_5/Task_1_hw_5/Source.c
@@ -1,50 +1,62 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
-
-int find(int sum, int index)
-{
-	int mas[] = { 1 , 2 , 5 , 10 , 20 , 50 , 100 , 200 };
-	int count = 0;
-
-	if ((sum < 0) || (index == -1))
-	{
-		 count = 0;
-	}
-	else if ((sum == 0) || (index == 0))
-	{
-		 count = 1;
-	}
-	else 
-	{
-		count = find(sum, index - 1) + find(sum - mas[index], index);
-	}
-	return count;
-}
-
-
-
+#include <stdlib.h>
+#define NUMBER_COUNTS 8
 
 int main()
 {
-	int sum = 0; int n = 0;
-	char eol = 0;
-	printf("The program solves the task \"English coin\"\n");
-	printf("Please, enter the number\n");
-	while (n == 0)
-	{
-		int sumscanf = scanf("%d%c", &sum, &eol);
-		if (sumscanf &&  eol == '\n' && sum > 0)
-		{
-			n = 1;
-			printf("%d", find(sum, 7));
-		}
-		else
-		{
-			printf("Error input. Try again\n");
-			scanf("%*[^\n]");
-		}
-	}
-		
-	return 0;
+    int coins[] = { 1, 2, 5, 10, 20, 50, 100, 200 };
+    int sum = 0; int n = 0;
+    char eol = 0;
+    printf("The program solves the task \"English coin\"\n");
+    printf("Please, enter the number\n");
+    while (n == 0)
+    {
+        int sumscanf = scanf("%d%c", &sum, &eol);
+        if (sumscanf && eol == '\n' && sum > 0)
+        {
+            n = 1;
+            long long int** mas = (long long int**)calloc(NUMBER_COUNTS + 1, sizeof(long long int*));
+            for (int i = 0; i <= NUMBER_COUNTS; i++)
+            {
+                mas[i] = (int*)calloc(sum + 1, sizeof(long long int));
+            }
+
+            for (int i = 0; i <= NUMBER_COUNTS; i++)
+                for (int j = 0; j <= sum; j++)
+                {
+                    mas[i][0] = 0;
+                    mas[0][j] = 1;
+                }
+
+            for (int i = 1; i < NUMBER_COUNTS; i++)
+            {
+                for (int j = 0; j <= sum; j++)
+                {
+                    if (j < coins[i])
+                    {
+                        mas[i][j] = mas[i - 1][j];
+                    }
+                    else
+                    {
+                        mas[i][j] = mas[i - 1][j] + mas[i][j - coins[i]];
+                    }
+                }
+            }
+
+            printf("result : %lld", mas[NUMBER_COUNTS - 1][sum]);
+            for (int i = 0; i < NUMBER_COUNTS; i++)
+            {
+                free(mas[i]);
+            }
+            free(mas);
+
+        }
+        else
+        {
+            printf("Error input. Try again\n");
+            scanf("%*[^\n]");
+        }
+    }
+    return 0;
 }
