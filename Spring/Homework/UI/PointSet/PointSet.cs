8cd32930d6a02da0232d2de62eae22d68aef8eba
kirill-ivanov-a <ivanov.krirll.a@mail.com>
2020-04-27 13:20:02 +0300

    added task "UI"

---
+++ Spring/Homework/UI/PointSet/PointSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace DrawingCurves
+{
+
+    public abstract class PointSet
+    {
+        //(x/a)^2 + (y/b)^2 = 1"
+        public string Name { get; }
+        public List<PointF[]> Points { get; protected set; }
+        protected int pointsPerSegment;
+        public int PixelsPerSegment
+        {
+            get
+            {
+                return pointsPerSegment;
+            }
+            protected set
+            {
+                if (value > 0)
+                    pointsPerSegment = value;
+                else
+                    throw new ArgumentException();
+            }
+        }
+
+        public List<PointF[]> GetPoints() => Points;
+
+        public PointSet(int pixelsPerSegment)
+        {
+            PixelsPerSegment = pixelsPerSegment;
+        }
+
+        public abstract void MakePoints();
+    }
+}
