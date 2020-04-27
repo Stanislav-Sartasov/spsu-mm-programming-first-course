5374977458f21b2ad54945dc42671c16fdf3dd2d
BakaevEvgeniy <evgeniybakaev@bk.ru>
2020-04-27 04:29:49 +0300

    task3 hashtable

---
+++ term 2/Task3HashTable/Task3HashTable/HashNode.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Task3HashTable
+{
+    public class HashNode<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+
+        public HashNode(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
---
+++ term 2/Task3HashTable/Task3HashTable/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Task3HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map<string, string> map = new Map<string, string>();
+
+            map.Add("SomeKey", "123Value");
+            map.Search("SomeKey");
+            map.Delete("SomeKey");
+            map.Search("SomeKey");
+
+
+
+        }
+    }
+}
