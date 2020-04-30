43709c8cec9031d78e41b344097830a176a23307
ToxaKaz <anton.a.kazancev@gmail.com>
2020-04-30 14:08:56 +0700

    Task6 complete

---
+++ Solutions_2_semester/Task6/Task6/Programm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task6
+{
+    public static class Programm
+    {
+        public static void Main() { }
+    }
+}
