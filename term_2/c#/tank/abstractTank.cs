c4c68d50e57ef3b03c0a3d45f68848374ab5f7f9
makar-pelogeiko <m.pelogeiko@mail.ru>
2020-05-06 16:56:53 +0300

    коммит из visual studio (задача казино + иерархия)

---
+++ term_2/c#/tank/abstractTank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tank
+{
+    public abstract class abstractTank
+    {
+        protected string title;
+        protected string contry;
+        protected int armor;
+        protected int gunNumber;
+        public virtual string getInfo()
+        {
+            return $" name: {title}\n contry: {contry}\n armor: {armor}mm\n number of guns: {gunNumber}";
+        }
+    }
+ }
