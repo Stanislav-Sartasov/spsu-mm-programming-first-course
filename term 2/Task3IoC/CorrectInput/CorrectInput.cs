8e921ae965eb01459350dd8701b97e8c8226f900
BakaevEvgeniy <evgeniybakaev@bk.ru>
2020-05-23 21:36:12 +0300

    Task3IoC

---
+++ term 2/Task3IoC/CorrectInput/CorrectInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CorrectInput
+{
+    public class CorrectInput
+    {
+        public static int IntInput(int input)
+        {
+            while (!(int.TryParse(Console.ReadLine(), out input)))
+                Console.WriteLine("Incorrect input! Try again.");
+            return input;
+        }
+    }
+}
