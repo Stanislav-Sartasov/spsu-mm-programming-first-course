c5116cd60a3a409c2a27116e3de721077e88611f
F <inkognito.kuzia>
2020-08-27 19:37:35 +0300

    - added "Simple Hierarchy"

---
+++ Homeworks/2 term/SecondTask/SecondTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TankTypes;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            HeavyTank Maus = new HeavyTank("Maus", "Germany", 1944, 128, 200, 260);
+            Maus.GetData();
+
+            LightTank LTTB = new LightTank("LTTB", "USSR", 1944, 85, 68, 33.95);
+            LTTB.GetData();
+
+            TankDestroyer Charioteer = new TankDestroyer("Charioteer", "UK", 1952, 84, 2590, 360);
+            Charioteer.GetData();
+        }
+    }
+}
