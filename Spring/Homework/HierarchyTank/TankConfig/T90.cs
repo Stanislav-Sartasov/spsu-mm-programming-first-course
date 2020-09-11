63783058ae6718d2d6bd1bc58d930c49da192469
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2020-09-11 12:51:23 +0300

    Hierarchy

--- Spring/Homework/HierarchyTank/TankConfig/T90.cs
+++ Spring/Homework/HierarchyTank/TankConfig/T90.cs
@@ -5,6 +5,7 @@ namespace TankConfig
     public class T90 : AbstractTank.AbstractTank
     {
         public T90() : base("T-90", "Russia", 46, new Dictionary<string, int> { { "Armor in millimeters", 65 }, { "Weapon caliber in millimeters", 125 }, { "Crew", 3 } })
-        { }
+        { 
+        }
     }
 }
