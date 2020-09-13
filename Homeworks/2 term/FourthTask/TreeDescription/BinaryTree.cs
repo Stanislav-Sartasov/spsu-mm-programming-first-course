66dc0e0d80e1a9799f25518761ca70bf5bd6f776
F <inkognito.kuzia>
2020-09-14 00:12:26 +0300

    fixed tabulation(hopefully)

--- Homeworks/2 term/FourthTask/TreeDescription/BinaryTree.cs
+++ Homeworks/2 term/FourthTask/TreeDescription/BinaryTree.cs
@@ -4,276 +4,236 @@ using System.Text;
 
 namespace TreeDescription
 {
-        public enum Side
-        {
-                Left,
-                Right
-        }
+	public enum Side
+	{
+		Left,
+		Right
+	}
 
-        public class BinaryTree<T>
-        {
-                public int? Key { get; private set; }
-                public T Data { get; private set; }
-                public BinaryTree<T> Left { get; private set; } // Left tree
-                public BinaryTree<T> Right { get; private set; } // Right tree
-                public BinaryTree<T> Parent { get; private set; } // Parent tree
-                private static Side? ParentSide(BinaryTree<T> node)
-                {
-                        if (node.Parent != null)
-                        {
-                                if (node.Parent.Left == node)
-                                {
-                                        return Side.Left;
-                                }
-                                if (node.Parent.Right == node)
-                                {
-                                        return Side.Right;
-                                }
-                        }
-                        return null;
-                }
+	public class BinaryTree<T>
+	{
+		public int? Key { get; private set; }
+		public T Data { get; private set; }
+		public BinaryTree<T> Left { get; private set; } // Left tree
+		public BinaryTree<T> Right { get; private set; } // Right tree
+		public BinaryTree<T> Parent { get; private set; } // Parent tree
+		private static Side? ParentSide(BinaryTree<T> node)
+		{
+			if (node.Parent != null)
+			{
+				if (node.Parent.Left == node)
+					return Side.Left;
+				if (node.Parent.Right == node)
+					return Side.Right;
+			}
+			return null;
+		}
 
-                public void AddElement(int key, T data, BinaryTree<T> node = null, BinaryTree<T> parent = null)
-                {
-                        if (node == null)
-                        {
-                                node = this;
-                        }
+		public void AddElement(int key, T data, BinaryTree<T> node = null, BinaryTree<T> parent = null)
+		{
+			if (node == null)
+				node = this;
 
-                        if (parent == null)
-                        {
-                                parent = this.Parent;
-                        }
+			if (parent == null)
+				parent = this.Parent;
 
-                        if (node.Key == null || node.Key == key)
-                        {
-                                if (node.Key == null)
-                                {
-                                        node.Left = null;
-                                        node.Right = null;
-                                }
-                                else
-                                {
-                                        node.Left = this.Left;
-                                        node.Right = this.Right;
-                                }
+			if (node.Key == null || node.Key == key)
+			{
+				if (node.Key == null)
+				{
+					node.Left = null;
+					node.Right = null;
+				}
+				else
+				{
+					node.Left = this.Left;
+					node.Right = this.Right;
+				}
 
-                                node.Key = key;
-                                node.Data = data;
-                                node.Parent = parent;
-                                return;
-                        }
+				node.Key = key;
+				node.Data = data;
+				node.Parent = parent;
+				return;
+			}
 
-                        if (node.Key > key)
-                        {
-                                if (node.Left == null)
-                                {
-                                        node.Left = new BinaryTree<T>();
-                                }
-                                AddElement(key, data, node.Left, node);
-                        }
-                        else
-                        {
-                                if (node.Right == null)
-                                {
-                                        node.Right = new BinaryTree<T>();
-                                }
-                                AddElement(key, data, node.Right, node);
-                        }
-                }
+			if (node.Key > key)
+			{
+				if (node.Left == null)
+					node.Left = new BinaryTree<T>();
+				AddElement(key, data, node.Left, node);
+			}
+			else
+			{
+				if (node.Right == null)
+					node.Right = new BinaryTree<T>();
+				AddElement(key, data, node.Right, node);
+			}
+		}
 
-                public void RemoveElement(int key)
-                {
+		public void RemoveElement(int key)
+		{
 
-                        var node = SearchElement(key);
+			var node = SearchElement(key);
 
-                        if (node == null)
-                        {
-                                return;
-                        }
+			if (node == null)
+				return;
 
-                        if (node == this)
-                        {
-                                BinaryTree<T> currNode;
-                                if (node.Right != null)
-                                {
-                                        currNode = node.Right;
-                                }
-                                else
-                                {
-                                        currNode = node.Left;
-                                }
+			if (node == this)
+			{
+				BinaryTree<T> currNode;
+				if (node.Right != null)
+					currNode = node.Right;
+				else
+					currNode = node.Left;
 
-                                while (currNode.Left != null)
-                                {
-                                        currNode = currNode.Left;
-                                }
+				while (currNode.Left != null)
+					currNode = currNode.Left;
 
-                                int tempKey = (int)currNode.Key;
-                                T tempData = currNode.Data;
+				int tempKey = (int)currNode.Key;
+				T tempData = currNode.Data;
 
-                                this.RemoveElement(tempKey);
+				this.RemoveElement(tempKey);
 
-                                node.Key = tempKey;
-                                node.Data = tempData;
-                                return;
-                        }
+				node.Key = tempKey;
+				node.Data = tempData;
+				return;
+			}
 
-                        if (node.Left == null && node.Right == null)
-                        {
-                                var nodeSide = ParentSide(node);
+			if (node.Left == null && node.Right == null)
+			{
+				var nodeSide = ParentSide(node);
 
-                                if (nodeSide == Side.Left)
-                                {
-                                        node.Parent.Left = null;
-                                }
-                                else
-                                {
-                                        node.Parent.Right = null;
-                                }
+				if (nodeSide == Side.Left)
+					node.Parent.Left = null;
+				else
+					node.Parent.Right = null;
 
-                                return;
-                        }
+				return;
+			}
 
-                        if (node.Left != null && node.Right == null)
-                        {
-                                var nodeSide = ParentSide(node);
+			if (node.Left != null && node.Right == null)
+			{
+				var nodeSide = ParentSide(node);
 
-                                if (nodeSide == Side.Left)
-                                {
-                                        node.Parent.Left = node.Left;
-                                }
-                                else
-                                {
-                                        node.Parent.Right = node.Left;
-                                }
+				if (nodeSide == Side.Left)
+				{
+					node.Parent.Left = node.Left;
+				}
+				else
+				{
+					node.Parent.Right = node.Left;
+				}
 
-                                node.Left.Parent = node.Parent;
-                                return;
-                        }
+				node.Left.Parent = node.Parent;
+				return;
+			}
 
-                        if (node.Left == null && node.Right != null)
-                        {
-                                var nodeSide = ParentSide(node);
+			if (node.Left == null && node.Right != null)
+			{
+				var nodeSide = ParentSide(node);
 
-                                if (nodeSide == Side.Left)
-                                {
-                                        node.Parent.Left = node.Right;
-                                }
-                                else
-                                {
-                                        node.Parent.Right = node.Right;
-                                }
+				if (nodeSide == Side.Left)
+				{
+					node.Parent.Left = node.Right;
+				}
+				else
+				{
+					node.Parent.Right = node.Right;
+				}
 
-                                node.Right.Parent = node.Parent;
-                                return;
-                        }
+				node.Right.Parent = node.Parent;
+				return;
+			}
 
-                        if (node.Right != null && node.Left != null)
-                        {
-                                var currNode = node.Right;
+			if (node.Right != null && node.Left != null)
+			{
+				var currNode = node.Right;
 
-                                while (currNode.Left != null)
-                                {
-                                        currNode = currNode.Left;
-                                }
+				while (currNode.Left != null)
+					currNode = currNode.Left;
 
-                                if (currNode.Parent == node)
-                                {
-                                        currNode.Left = node.Left;
-                                        node.Left.Parent = currNode;
-                                        currNode.Parent = node.Parent;
+				if (currNode.Parent == node)
+				{
+					currNode.Left = node.Left;
+					node.Left.Parent = currNode;
+					currNode.Parent = node.Parent;
 
-                                        var nodeSide = ParentSide(node);
+					var nodeSide = ParentSide(node);
 
-                                        if (nodeSide == Side.Left)
-                                        {
-                                                node.Parent.Left = currNode;
-                                        }
-                                        else
-                                        {
-                                                node.Parent.Right = currNode;
-                                        }
+					if (nodeSide == Side.Left)
+						node.Parent.Left = currNode;
+					else
+						node.Parent.Right = currNode;
 
-                                        return;
-                                }
-                                else
-                                {
-                                        if (currNode.Right != null)
-                                        {
-                                                currNode.Right.Parent = currNode.Parent;
-                                        }
+					return;
+				}
+				else
+				{
+					if (currNode.Right != null)
+						currNode.Right.Parent = currNode.Parent;
 
-                                        currNode.Parent.Left = currNode.Right;
-                                        currNode.Right = node.Right;
-                                        currNode.Left = node.Left;
-                                        node.Left.Parent = currNode;
-                                        node.Right.Parent = currNode;
-                                        currNode.Parent = node.Parent;
+					currNode.Parent.Left = currNode.Right;
+					currNode.Right = node.Right;
+					currNode.Left = node.Left;
+					node.Left.Parent = currNode;
+					node.Right.Parent = currNode;
+					currNode.Parent = node.Parent;
 
-                                        if (node == node.Parent.Left)
-                                        {
-                                                node.Parent.Left = currNode;
-                                        }
-                                        else
-                                        {
-                                                node.Parent.Right = currNode;
-                                        }
+					if (node == node.Parent.Left)
+					{
+						node.Parent.Left = currNode;
+					}
+					else
+						node.Parent.Right = currNode;
 
-                                        return;
-                                }
-                        }
-                }
+					return;
+				}
+			}
+		}
 
-                public BinaryTree<T> SearchElement(int key, /*int isPrint = 0,*/ BinaryTree<T> searchNode = null)
-                {
-                        if (searchNode == null)
-                        {
-                                searchNode = this;
-                        }
+		public BinaryTree<T> SearchElement(int key, /*int isPrint = 0,*/ BinaryTree<T> searchNode = null)
+		{
+			if (searchNode == null)
+				searchNode = this;
 
-                        if (searchNode.Key == key)
-                        {
-                                /*
-                                if (isPrint == 1)
-                                    Console.WriteLine($"{searchNode.Key}: {searchNode.Data}");
-                                */
+			if (searchNode.Key == key)
+			{
+				/*
+				if (isPrint == 1)
+				    Console.WriteLine($"{searchNode.Key}: {searchNode.Data}");
+				*/
 
-                                return searchNode;
-                        }
+				return searchNode;
+			}
 
-                        if (searchNode.Key > key)
-                        {
-                                if (searchNode.Left != null)
-                                {
-                                        return SearchElement(key, /*isPrint,*/ searchNode.Left);
-                                }
-                                else
-                                {
-                                        /*
-                                        if (isPrint == 1)
-                                            Console.WriteLine("Not found");
-                                        */
+			if (searchNode.Key > key)
+			{
+				if (searchNode.Left != null)
+					return SearchElement(key, /*isPrint,*/ searchNode.Left);
+				else
+				{
+					/*
+					if (isPrint == 1)
+					    Console.WriteLine("Not found");
+					*/
 
-                                        return null;
-                                }
-                        }
-                        else
-                        {
-                                if (searchNode.Right != null)
-                                {
-                                        return SearchElement(key, /*isPrint,*/ searchNode.Right);
-                                }
-                                else
-                                {
-                                        /*
-                                        if (isPrint == 1)
-                                            Console.WriteLine("Not found");
-                                        */
+					return null;
+				}
+			}
+			else
+			{
+				if (searchNode.Right != null)
+					return SearchElement(key, /*isPrint,*/ searchNode.Right);
+				else
+				{
+					/*
+					if (isPrint == 1)
+					    Console.WriteLine("Not found");
+					*/
 
-                                        return null;
-                                }
-                        }
-                }
-        }
+					return null;
+				}
+			}
+		}
+	}
 }
