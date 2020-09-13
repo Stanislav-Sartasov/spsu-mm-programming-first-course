c7bb981f22480eccece0d649a896f269ef03db6f
F <inkognito.kuzia>
2020-09-14 00:03:04 +0300

    changed tabulation(hopefully)

--- Homeworks/2 term/SecondTask/TankCommonTemplate/TankTemplate.cs
+++ Homeworks/2 term/SecondTask/TankCommonTemplate/TankTemplate.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 
-namespace TankDescription
+namespace TankTemplateDescription
 {
-    public abstract class Tank
-    {
-        public string Name { get; set; }
-        public string Country { get; set; }
-        public int ProductionYear { get; set; }
-        public int GunCaliber { get; set; }
-        public Tank(string name, string country, int year, int caliber)
-        {
-            Name = name;
-            Country = country;
-            ProductionYear = year;
-            GunCaliber = caliber;
-        }
-        public virtual string GetData()
-        {
-            string output = $"Tank description:\nName: {Name}, country: {Country}, production year: {ProductionYear}, gun caliber: {GunCaliber} mm, ";
-            Console.Write(output);
-            return output;
-        }
-    }
+	public abstract class Tank
+	{
+		public string Name { get; set; }
+		public string Country { get; set; }
+		public int ProductionYear { get; set; }
+		public int GunCaliber { get; set; }
+		public Tank(string name, string country, int year, int caliber)
+		{
+			Name = name;
+			Country = country;
+			ProductionYear = year;
+			GunCaliber = caliber;
+		}
+		public virtual string GetData()
+		{
+			string output = $"Tank description:\nName: {Name}, country: {Country}, production year: {ProductionYear}, gun caliber: {GunCaliber} mm, ";
+			Console.Write(output);
+			return output;
+		}
+	}
 }
