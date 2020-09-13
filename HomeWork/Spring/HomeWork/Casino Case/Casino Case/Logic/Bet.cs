3bee1036dd1a2b0e0d5a7ba95f845c97f10e8d88
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2020-09-13 15:54:50 +0300

    modifiers changed

--- HomeWork/Spring/HomeWork/Casino Case/Casino Case/Logic/Bet.cs
+++ HomeWork/Spring/HomeWork/Casino Case/Casino Case/Logic/Bet.cs
@@ -6,21 +6,21 @@ namespace Casino_Case.Logic
 {
     abstract public class Bet
     {
-        internal void InputChecking(int TBid)
+        protected void InputChecking(int TBid)
         {
             if (TBid > BetInfo.balance)
                 throw new Exception("You dont have enough money");
             if (TBid <= 0)
                 throw new Exception("Bid can't be less than 1");
         }
-        internal void OutputWin(int TBid, int TCoef)
+        protected void OutputWin(int TBid, int TCoef)
         {
             Console.WriteLine("You've just won " + TBid * TCoef);
             BetInfo.balance += (TBid) * TCoef;
             BetInfo.profit += (TBid) * TCoef;
             BetInfo.AmountOfBets++;
         }
-        internal void OutputLose(int TBid)
+        protected void OutputLose(int TBid)
         {
             Console.WriteLine("Didn't win :(");
             BetInfo.balance -= TBid;
