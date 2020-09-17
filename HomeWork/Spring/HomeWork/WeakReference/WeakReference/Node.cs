531c18e8689c3a7a43266908d1882a60e4c6b91d
KostennikovDanil <55431913+KostennikovDanil@users.noreply.github.com>
2020-09-17 12:54:03 +0300

    task added

---
+++ HomeWork/Spring/HomeWork/WeakReference/WeakReference/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeakReference
+{
+    public class Node<T> where T: class
+    {
+        public Node(int _key, T _value)
+        {
+            key = _key;
+            value = new WeakReference<T>(_value);
+        }
+        public bool IsAlive()
+        {
+            return value.TryGetTarget(out T target);
+        }
+        public T GetValue()
+        {
+            value.TryGetTarget(out T target);
+            return target;
+        }
+        public void SetValue(T newValue)
+        {
+            value.SetTarget(newValue);
+        }
+
+
+
+        public WeakReference<T> value;
+        public int key;
+        public Node<T> left;
+        public Node<T> right;
+        public void DisplayNode()
+        {
+            Console.WriteLine(key + " ");
+        }
+    }
+}
