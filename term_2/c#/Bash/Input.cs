f9ad865ff0774bd4209603aef725e52dd6c33c03
makar-pelogeiko <m.pelogeiko@mail.ru>
2020-09-24 04:38:32 +0300

    "Bash" правки кодстайла

--- term_2/c#/Bash/Input.cs
+++ term_2/c#/Bash/Input.cs
@@ -193,31 +193,5 @@ namespace Bash
             st = Status.arg;
         }
     }
-    public class Command : Message
-    {
-        public readonly string cmd;
-        public Command(string start)
-        {
-            interup = Interup.queue;
-            cmd = start;
-            st = Status.cmd;
-        }
-        internal Command(string start, Interup myInterup)
-        {
-            interup = myInterup;
-            cmd = start;
-            st = Status.cmd;
-        }
-    }
-    public interface IInteraction
-    {
-        string GetStr();
-    }
-    class Interaction : IInteraction
-    {
-        public string GetStr()
-        {
-            return Console.ReadLine();
-        }
-    }
+    
 }
