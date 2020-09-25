6fd91eafb517d5866c46d470c9565b0af063be57
David <bykov-david@mail.ru>
2020-09-25 19:17:32 +0300

    -Простая иерархия

---
+++ Tasks/2ndSemester/SimpleHierarchy/SimpleHierarchy.ClassesHeirs/BananaIceCream.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SimpleHierarchy.Origin;
+
+namespace SimpleHierarchy.ClassesHeirs
+{
+    public class BananaIceCream : AbstractOriginIceCream
+    {
+        public int Bananas { get; protected set; }
+
+        public BananaIceCream()
+        {
+            IceCreamName = "Banana Ice Cream";
+            Weight = 150;
+            Milk = 60;
+            Eggs = 2;
+            Sugar = 10;
+            Cream = 30;
+            Bananas = 1;
+        }
+
+        public override string ShowRecipe()
+        {
+            string recipe = base.ShowRecipe() + $" Number of bananas - {Bananas}\n";
+            return recipe;
+        }
+    }
+}
---
+++ Tasks/2ndSemester/SimpleHierarchy/SimpleHierarchy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SimpleHierarchy.ClassesHeirs;
+
+namespace SimpleHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BananaIceCream bananaIceCream = new BananaIceCream();
+            Console.WriteLine(bananaIceCream.ShowRecipe());
+
+            ChocolateIceCream chocolateIceCream = new ChocolateIceCream();
+            Console.WriteLine(chocolateIceCream.ShowRecipe());
+        }
+    }
+}
