9d8157ffc28992fff46f13872483be3d4e1b6887
Ilias Mzoughi <i7ok@yandex.ru>
2020-09-26 16:06:45 +0300

    reuploaded

---
+++ Homework/HW10EnglishCoins/main.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+	int numberOfPences;
+
+	printf("Enter pences quantity: ");
+
+	scanf("%d", &numberOfPences);
+
+	int i, j;
+	int coins[8] = { 1, 2, 5, 10, 20, 50, 100, 200 };
+	unsigned long long* tempArr;
+
+	tempArr = (unsigned long long*)calloc((numberOfPences + 1), sizeof(unsigned long long));
+	tempArr[0] = 1;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = coins[i]; j < numberOfPences + 1; j++)
+		{
+			tempArr[j] = tempArr[j] + tempArr[j - coins[i]];
+		}
+	}
+
+	printf("\nNumber of variants: %lld", tempArr[numberOfPences]);
+
+	return(0);
+}
