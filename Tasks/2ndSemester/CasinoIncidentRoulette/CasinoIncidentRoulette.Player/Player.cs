18ff2de4580babf4a3fa5a1c1e11c9b4d074acd4
David <bykov-david@mail.ru>
2020-09-27 15:56:02 +0300

    -Случай в казино

---
+++ Tasks/2ndSemester/CasinoIncidentRoulette/CasinoIncidentRoulette.Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using CasinoIncidentRoulette.Roulette;
+
+namespace CasinoIncidentRoulette.Player
+{
+    public abstract class AbstractPlayer
+    {
+        public int Money { get; protected set; }
+        public List<Table.Cell> History { get; protected set; }
+        public abstract Tuple<Table.Cell, int, int> Bet();
+        public abstract void CheckResult(Tuple<Table.Cell, int, int> cellBet, Table.Cell exodus);
+        public int GetMoney()
+        {
+            return Money;
+        }
+        public void AddMoney(int amount)
+        {
+            Money += amount;
+        }
+
+    }
+}
---
+++ Tasks/2ndSemester/CasinoIncidentRoulette/CasinoIncidentRoulette/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CasinoIncidentRoulette.Roulette;
+using CasinoIncidentRoulette.Bots;
+
+namespace CasinoIncidentRoulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table.CreateTable();
+            Table.Cell exodus;
+
+            MartingaleBot martingaleBot = new MartingaleBot();
+            Tuple<Table.Cell, int, int> martingaleBet = martingaleBot.Bet();
+
+            MakarovBot makarovBot = new MakarovBot();
+            Tuple<Table.Cell, int, int> makarovBet = makarovBot.Bet();
+
+            for (int i = 0; i < 400; i++)
+            {
+                if (martingaleBot.CanIBet())
+                    martingaleBet = martingaleBot.Bet();
+                if (makarovBot.CanIBet())
+                    makarovBet = makarovBot.Bet();
+
+                exodus = Table.Roll();
+
+                if (martingaleBot.CanIBet())
+                    martingaleBot.CheckResult(martingaleBet, exodus);
+                if (makarovBot.CanIBet())
+                    makarovBot.CheckResult(makarovBet, exodus);
+            }
+
+            if (martingaleBot.CanIBet())
+                Console.WriteLine($"Martingale bot didn't lost. Money: {martingaleBot.GetMoney()}");
+            else
+                Console.WriteLine($"Martingale bot lost. Money: {martingaleBot.GetMoney()}");
+
+            if (makarovBot.CanIBet())
+                Console.WriteLine($"Martingale bot didn't lost. Money: {makarovBot.GetMoney()}");
+            else
+                Console.WriteLine($"Martingale bot lost. Money: {makarovBot.GetMoney()}");
+        }
+    }
+}
