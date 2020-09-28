0d2d8acba365c7401080676812aa143a659b3ad1
Ilias Mzoughi <i7ok@yandex.ru>
2020-09-28 09:51:51 +0300

    Fixed

--- Homework/HW4MersenneNumbers/HW4MersenneNumbers/main.c
+++ Homework/HW4MersenneNumbers/HW4MersenneNumbers/main.c
@@ -1,14 +1,14 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 
 int mersenneNumber()
 {
 	int numb;
-	for (int n = 1; n < 32; n++)
+	for (int n = 2; n < 32; n++)
 	{
 		int prime = 0;
 		numb = pow(2, n) - 1;
-		for (int j = 2; j < (int)sqrt(numb); j++)
+		for (int j = 2; j < sqrt(numb); j++)
 			if (numb % j == 0)
 				prime = 1;
 		if (prime == 0)
