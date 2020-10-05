69e5fa351c6d90686be32f5990b8e23bf76e655b
makar-pelogeiko <m.pelogeiko@mail.ru>
2020-10-05 03:21:17 +0300

    задача "ui" решение на WindowsForms + библиотека кривых

---
+++ term_2/c#/Ui/MathCruves/Cruve.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MathCruves
+{
+    public abstract class Cruve
+    {
+        public string cruve;
+        public virtual List<PointF> Raschet(List<float> lst, out bool error)
+        {
+            error = false;
+            return new List<PointF>();
+        }
+        public override string ToString()
+        {
+            return cruve;
+        }
+    }
+}
