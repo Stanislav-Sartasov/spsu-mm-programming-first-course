344832ff0889cfd2b7a8a7f4220f220346a91e6b
Ilias Mzoughi <i7ok@yandex.ru>
2020-10-27 00:45:21 +0300

    HW12 reworked.

--- Homework/HW12LongArr/Long Arr/main.c
+++ Homework/HW12LongArr/Long Arr/main.c
@@ -5,31 +5,40 @@
 
 void powerArithmetics (uint8_t number, unsigned short int power)
 {
-	unsigned short int hexDigitsCount = (1 + (log(number) / log(16)) * power);
+	unsigned short int hexDigitsCount = (1 + (log(number) / log(256)) * power);
 	uint8_t* hexDigits;
-	hexDigits = (uint8_t*)malloc(hexDigitsCount * sizeof(uint8_t));
-	
+	hexDigits = (uint8_t*)malloc(hexDigitsCount);
+	unsigned int temp, hexDigitTail = 0;
+
 	for (unsigned short int i = 0; i < hexDigitsCount; i++)
 	{
 		hexDigits[i] = 0;
 	} 
+
 	hexDigits[0] = 1;
+
 	for (unsigned short int i = 0; i < power; i++)
 	{
 		for (unsigned short int j = 0; j < hexDigitsCount; j++)
-			hexDigits[j] = hexDigits[j] * 3;
-
-		for (unsigned short int j = 0; j < hexDigitsCount - 1; j++)
 		{
-			if (hexDigits[j] > 15)
+			if ((unsigned int)hexDigits[j] * 3 + hexDigitTail > 255)
 			{
-				hexDigits[j + 1] += hexDigits[j] / 16;
-				hexDigits[j] %= 16;
+				temp = hexDigits[j] * 3 + hexDigitTail;
+				hexDigits[j] = temp % 256;
+				hexDigitTail = temp / 256;
+			}
+			else
+			{
+				hexDigits[j] = hexDigits[j] * 3 + hexDigitTail;
+				hexDigitTail = 0;
 			}
 		}
 	}
 	for (short int i = hexDigitsCount - 1; i >= 0; i--)
-		printf("%x", hexDigits[i]);
+	{
+		printf("%x", hexDigits[i] / 16);
+		printf("%x", hexDigits[i] % 16);
+	}
 	free(hexDigits);
 }
 
