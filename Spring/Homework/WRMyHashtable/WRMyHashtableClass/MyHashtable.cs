a3f0a34fb05e470b87d2c59ae2830c09ce2da0bb
Lev Chernishev <44917208+LevChernishev@users.noreply.github.com>
2020-11-19 18:59:48 +0300

    WR

--- Spring/Homework/WRMyHashtable/WRMyHashtableClass/MyHashtable.cs
+++ Spring/Homework/WRMyHashtable/WRMyHashtableClass/MyHashtable.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Item;
 
 namespace WRMyHashTableClass
 {
@@ -177,38 +177,6 @@ namespace WRMyHashTableClass
 			{
 				Console.WriteLine(item);
 			}
-		}
-	}
-
-	public class Item<TKey, TValue> where TValue : class
-	{
-		public TKey Key { get; private set; }
-		public WeakReference<TValue> Value { get; private set; }
-		private int storageTime;
-		public Item(int storageTime, TKey key, TValue value)
-		{
-			this.storageTime = storageTime;
-			SetPair(key, value);
-		}
-
-		public Item(TKey key, TValue value)
-		{
-			Key = key;
-			Value = new WeakReference<TValue>(value);
-		}
-		async public void SetPair(TKey key, TValue value)
-		{
-			Key = key;
-			Value = new WeakReference<TValue>(value);
-			await Task.Delay(storageTime);
-		}
-
-		public override string ToString()
-		{
-			if (Value.TryGetTarget(out TValue target))
-				return $"{Key}, {target}";
-			else
-				return $"{Key}, collected";
 		}
 	}
 }
