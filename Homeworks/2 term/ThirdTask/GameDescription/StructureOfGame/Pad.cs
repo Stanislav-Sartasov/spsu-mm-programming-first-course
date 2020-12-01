d94c9dd6c00d2d8e5018b172d7c92f7a5ce63d4b
F <inkognito.kuzia>
2020-12-01 11:38:55 +0300

    added little description

--- Homeworks/2 term/ThirdTask/GameDescription/StructureOfGame/Pad.cs
+++ Homeworks/2 term/ThirdTask/GameDescription/StructureOfGame/Pad.cs
@@ -6,6 +6,6 @@ namespace GameDescription
 {
 	public class Pad
 	{
-		public int[] cards = new int[12] { 0, 0, 32, 32, 32, 32, 32, 32, 32, 32, 128, 32 };
+		public int[] cards = new int[12] { 0, 0, 32, 32, 32, 32, 32, 32, 32, 32, 128, 32 }; // It is assumed that at a nominal value of 10 the number of cards of each dignity (10, king, queen, jack) is less than 32
 	}
 }
