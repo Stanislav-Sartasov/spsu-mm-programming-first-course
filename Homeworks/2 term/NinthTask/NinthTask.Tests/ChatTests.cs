0c3d285cb0e56faaf388f7f99ca24d313717c7e0
F <inkognito.kuzia>
2021-03-03 10:58:05 +0300

    completely rewrote chat (now using UDP, P2P connection + tests) note: fixing bugs and adding some features still in progress

--- Homeworks/2 term/NinthTask/NinthTask.Tests/ChatTests.cs
+++ Homeworks/2 term/NinthTask/NinthTask.Tests/ChatTests.cs
@@ -1,14 +1,133 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatDescription;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace NinthTask.Tests
 {
-	[TestClass]
-	public class ChatTests
-	{
-		[TestMethod]
-		public void ChatTestMethod()
+        [TestClass]
+        public class ChatTests
+        {
+                Client firstUser;
+                Client secondUser;
+                Client thirdUser;
+                IPEndPoint ipF;
+                IPEndPoint ipS;
+                IPEndPoint ipT;
+
+                [TestInitialize] //переписать в обычный метод из-за исключения сокетов
+                public void TestInit()
+                {
+                        firstUser = new Client();
+                        secondUser = new Client();
+                        thirdUser = new Client();
+
+                        ipF = IPInit(11111);
+                        ipS = IPInit(22222);
+                        ipT = IPInit(33333);
+                }
+
+                [TestMethod]
+                public void TwoConnectionTest()
+                {
+                        firstUser.TryGetAddress(ipF);
+                        secondUser.TryGetAddress(ipS);
+
+                        //firstUser.StartListening();
+                        secondUser.StartListening();
+
+                        Assert.AreEqual(11111, firstUser.ClientIP.Port);
+                        Assert.AreEqual(22222, secondUser.ClientIP.Port);
+                       
+                        firstUser.Connect(secondUser.ClientIP);
+
+                        Thread.Sleep(1);
+
+                        var actualFirst = Connections(firstUser);
+                        Assert.AreEqual(1, actualFirst.Count);
+                        Assert.AreEqual(22222, actualFirst[0].Port);
+                        Assert.AreEqual(secondUser.ClientIP.Address, actualFirst[0].Address);
+
+                        Thread.Sleep(1);
+
+                        var actualSecond = Connections(secondUser);
+                        Assert.AreEqual(1, actualSecond.Count);
+                        Assert.AreEqual(11111, actualSecond[0].Port);
+                        Assert.AreEqual(firstUser.ClientIP.Address, actualSecond[0].Address);
+                }
+
+                [TestMethod]
+                public void ThreeConnectionTest()
 		{
-			//in progress
+                        firstUser.TryGetAddress(ipF);
+                        secondUser.TryGetAddress(ipS);
+                        thirdUser.TryGetAddress(ipT);
+
+                        //firstUser.StartListening();
+                        secondUser.StartListening();
+                        thirdUser.StartListening();
+
+                        Assert.AreEqual(33333, thirdUser.ClientIP.Port);
+
+                        //firstUser.Connect(thirdUser.ClientIP);
+                        thirdUser.Connect(firstUser.ClientIP);
+
+                        Thread.Sleep(1);
+
+                        var actualFirst = Connections(firstUser);
+                        Assert.AreEqual(2, actualFirst.Count);
+                        Assert.AreEqual(22222, actualFirst[0].Port);
+                        Assert.AreEqual(33333, actualFirst[1].Port);
+                        Assert.AreEqual(secondUser.ClientIP.Address, actualFirst[0].Address);
+                        Assert.AreEqual(thirdUser.ClientIP.Address, actualFirst[1].Address);
+
+                        var actualThird = new List<IPEndPoint>(thirdUser.UserList); //вынеси отдельно
+                        Assert.AreEqual(2, actualThird.Count);
+                        Assert.AreEqual(11111, actualThird[0].Port);
+                        Assert.AreEqual(22222, actualThird[1].Port);
+                        Assert.AreEqual(firstUser.ClientIP.Address, actualThird[0].Address);
+                        Assert.AreEqual(secondUser.ClientIP.Address, actualThird[1].Address);
+                }
+
+                [TestMethod]
+                public void DisconnectionTest()
+		{
+                        firstUser.TryGetAddress(ipF);
+                        secondUser.TryGetAddress(ipS);
+                        thirdUser.TryGetAddress(ipT);
+
+                        firstUser.StartListening();
+                        secondUser.StartListening();
+                        thirdUser.StartListening();
+
+                        firstUser.Disconnect();
+
+                        Thread.Sleep(1);
+
+                        var actualThird = Connections(thirdUser);
+                        Assert.AreEqual(1, actualThird.Count);
+                        Assert.AreEqual(101, actualThird[0].Port);
+                        Assert.AreEqual(secondUser.ClientIP.Address, actualThird[0].Address);
+                }
+
+                private static List<IPEndPoint> Connections(Client client)
+		{
+                        return new List<IPEndPoint>(client.UserList);
 		}
-	}
+
+                private static IPEndPoint IPInit(int port)
+		{
+                        foreach (var iPAdress in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+                        {
+                                if (iPAdress.AddressFamily == AddressFamily.InterNetwork)
+                                {
+                                        return new IPEndPoint(iPAdress, port);
+                                }
+                        }
+
+                        return null;
+                }
+        }
 }
--- Homeworks/2 term/NinthTask/Program.cs
+++ Homeworks/2 term/NinthTask/Program.cs
@@ -1,39 +1,19 @@
 ﻿using System;
-using NinthTask.ChatDescription;
+using ChatDescription;
 
 namespace NinthTask
 {
 	class Program
 	{
-		#region usage
-
-		/*
-		No parameters - only server
-		Connection: tasksocket.exe <ipaddress> <port>
-		tasksocket.exe <port> == tasksocket.exe 127.0.0.1 <port>
-		 */
-
-		#endregion
-		static void Main(string[] args)
+		static void Main()
 		{
-			var chat = new ChatManager();
-
-			if (args.Length == 0)
-			{
-				chat.StartServer(); //сервер - для себя, поменяю
-			}
-			else if (args.Length == 1)
-			{
-				chat.StartChatting(args[0]);
-			}
-			else if (args.Length == 2)
-			{
-				chat.StartChatting(args[0], args[1]);
-			}
-			else
-			{
-				Console.WriteLine("Incorrect input format.");
-			}
+			var chat = new ChatManager(new Client());
+			chat.StartChating();
 		}
 	}
 }
+
+//TODO:
+//добавить имена
+//исправить ошибки
+//доделать тесты
