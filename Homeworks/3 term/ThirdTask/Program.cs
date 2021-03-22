b8e6cb3445a8afb8b5221ee06ae23c3757370c32
F <inkognito.kuzia>
2021-03-22 13:54:59 +0300

    added task 3

---
+++ Homeworks/3 term/ThirdTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ThirdTask
+{
+	class Program
+	{
+		private const int producersNum = 3;
+		private const int consumersNum = 3;
+
+		static void Main()
+		{
+			Console.WriteLine("The program has started!");
+
+			var manager = new Manager(producersNum, consumersNum);
+
+			manager.Start(0);
+			manager.Start(1);
+			
+			Console.ReadKey();
+
+			manager.Finish(0);
+			manager.Finish(1);
+
+			Console.WriteLine("The program has finished!");
+			//Console.ReadKey();
+		}
+
+	}
+}
