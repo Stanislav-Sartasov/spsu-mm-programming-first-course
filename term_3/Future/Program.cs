e69be69ef9ceda080ca382c4d0d7a99248e52f04
makar-pelogeiko <m.pelogeiko@mail.ru>
2021-03-26 01:49:50 +0300

    задача Future 1ая схема распараллеливания (каскадная)

---
+++ term_3/Future/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FutureLibrary;
+namespace Future
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVectorLengthComputer vector = new Cascade();
+            int[] a = { 3, 4 };
+            double result = vector.ComputeLength(a);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ term_3/Future/UnitTestFuture/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using FutureLibrary;
+
+namespace UnitTestFuture
+{
+    [TestClass]
+    public class UnitTest
+    {
+        int[] testArray = { 3, 4 };
+        double TestResult;
+        [TestInitialize]
+        public void Initialize()
+        {
+            TestResult = Math.Sqrt(testArray.Sum((i) => i * i));
+        }
+        [TestMethod]
+        public void TestCascade()
+        {
+            IVectorLengthComputer vector = new Cascade();
+            double result = vector.ComputeLength(testArray);
+            Assert.AreEqual(TestResult, result);
+        }
+    }
+}
