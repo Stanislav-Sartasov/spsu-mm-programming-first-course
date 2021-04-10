f737938fc8765034a2fb00be4c3d25d5187691fe
ToxaKaz <anton.a.kazancev@gmail.com>
2021-04-10 22:25:13 +0300

    Task6 added

---
+++ Solutions_3_semester/Task6/Task6/LazySet/Elem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task6.LazySet
+{
+	class Elem<T>
+	{
+		public Locker Locker { get; } = new Locker();
+		public int Key { get; set; } = int.MinValue;
+		public T Value { get; set; } = default;
+		public Elem<T> Next { get; set; } = null;
+		public bool Marked { get; set; } = false;
+	}
+}
---
+++ Solutions_3_semester/Task6/Task6/RefinableHashSet/Bin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task6;
+using System.Collections;
+
+namespace Task6.RefinableHashSet
+{
+	class Bin
+	{
+		public Locker Locker { get; } = new Locker();
+		HashSet<(long, long)> content = new HashSet<(long, long)>();
+
+		public void Add(long studentId, long courseId)
+		{
+			Locker.Lock();
+			content.Add((studentId, courseId));
+			Locker.Unlock();
+		}
+		public void Remove(long studentId, long courseId)
+		{
+			Locker.Lock();
+			content.Remove((studentId, courseId));
+			Locker.Unlock();
+		}
+		public bool Contains(long studentId, long courseId)
+		{
+			Locker.Lock();
+			bool result = content.Contains((studentId, courseId));
+			Locker.Unlock();
+			return result;
+		}
+	}
+}
